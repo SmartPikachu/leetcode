fd5ac4b29ffc4e63ac39c6a9f49c637beef66226
Pikachu <jiangjiaxin2011@126.com>
2022-03-09 17:39:03 +0800

    动态规划提交2

---
+++ src/main/java/cn/jjx/coding/leetcode/labuladong/sliding_window/LC72_编辑距离.java
@@ -0,0 +1,30 @@
+package cn.jjx.coding.leetcode.labuladong.sliding_window;
+
+public class LC72_编辑距离 {
+    public int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+        int[][] dp = new int[n+1][m+1];
+        if(m*n==0){
+            return m+n;
+        }
+        for(int i=0;i<=n;i++){
+            dp[i][0]=i;
+        }
+        for(int j=0;j<=m;j++){
+            dp[0][j]=j;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                int down = dp[i-1][j]+1;
+                int right = dp[i][j-1]+1;
+                int down_right = dp[i-1][j-1];
+                if(word1.charAt(i-1) != word2.charAt(j-1)){
+                    down_right+=1;
+                }
+                dp[i][j] = Math.min(down,Math.min(right,down_right));
+            }
+        }
+        return dp[n][m];
+    }
+}
