be1a3b40111956f1329374f8c3924330acd8fa20
Pikachu <jiangjiaxin2011@126.com>
2022-03-31 22:35:56 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/aatest/Solution.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/aatest/Solution.java
@@ -1,7 +1,14 @@
 package cn.jjx.coding.leetcode.classification.aatest;
 
+import java.util.Arrays;
+
 public class Solution {
     public static void main(String[] args) {
+        char[][] chars = new char[2][2];
+        for(int i=0;i<2;i++){
+            Arrays.fill(chars[i],'.');
+        }
+
 
     }
 }
