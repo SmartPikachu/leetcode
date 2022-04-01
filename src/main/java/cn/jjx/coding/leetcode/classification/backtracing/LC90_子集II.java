51e52fd8bb4275f5e6c308274d9c4b8e35e782a6
Pikachu <jiangjiaxin2011@126.com>
2022-04-01 22:00:31 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/backtracing/LC90_子集II.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/backtracing/LC90_子集II.java
@@ -29,4 +29,34 @@ public class LC90_子集II {
         t.remove(t.size() - 1);
     }
 
+
+    //下面是自己写的
+    public List<List<Integer>> subsetsWithDup1(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if(nums==null || nums.length==0) return ans;
+        List<Integer> temp = new ArrayList<Integer>();
+        boolean[] visited = new boolean[nums.length];
+        Arrays.sort(nums);
+        backtrace(ans,temp,nums,0,visited);
+        return ans;
+    }
+
+    public void backtrace(List<List<Integer>> ans,List<Integer> temp,int[] nums,int idx,boolean[] visited){
+        if(idx==nums.length){
+            ans.add(new ArrayList<Integer>(temp));
+            return;
+        }
+        backtrace(ans,temp,nums,idx+1,visited);
+
+        if(idx>0 && nums[idx-1]==nums[idx] && !visited[idx-1]){
+            return;
+        }
+
+        visited[idx]=true;
+        temp.add(nums[idx]);
+        backtrace(ans,temp,nums,idx+1,visited);
+        temp.remove(temp.size()-1);
+        visited[idx]=false;
+    }
+
 }
