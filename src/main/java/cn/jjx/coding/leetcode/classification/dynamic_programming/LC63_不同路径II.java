807e2970b4cfc8572ee4e4f25617ae16e60c96d9
Pikachu <jiangjiaxin2011@126.com>
2022-04-12 18:20:44 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/dynamic_programming/LC63_不同路径II.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/dynamic_programming/LC63_不同路径II.java
@@ -22,4 +22,25 @@ public class LC63_不同路径II {
         return f[m - 1];
     }
 
+    public int uniquePathsWithObstacles1(int[][] obstacleGrid) {
+        int n=obstacleGrid.length, m=obstacleGrid[0].length;
+        int[][] dp = new int[n][m];
+        dp[0][0]=obstacleGrid[0][0]==0?1:0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(obstacleGrid[i][j]==1){
+                    dp[i][j]=0;
+                    continue;
+                }
+                if(i-1>=0 && obstacleGrid[i-1][j]==0){
+                    dp[i][j]+=dp[i-1][j];
+                }
+                if(j-1>=0 && obstacleGrid[i][j-1]==0){
+                    dp[i][j]+=dp[i][j-1];
+                }
+            }
+        }
+        return dp[n-1][m-1];
+    }
+
 }
