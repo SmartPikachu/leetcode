0e3c26ec5439e24676a48ce341b6f4d5e443fcc4
Pikachu <jiangjiaxin2011@126.com>
2022-04-15 17:37:25 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/dynamic_programming/LC337_打家劫舍III.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/dynamic_programming/LC337_打家劫舍III.java
@@ -24,4 +24,24 @@ public class LC337_打家劫舍III {
         g.put(node, Math.max(f.getOrDefault(node.left, 0), g.getOrDefault(node.left, 0)) + Math.max(f.getOrDefault(node.right, 0), g.getOrDefault(node.right, 0)));
     }
 
+
+
+
+    //仅仅是优化了没有使用hashmap而已
+    public int rob1(TreeNode root) {
+        int[] rootStatus = dfs1(root);
+        return Math.max(rootStatus[0], rootStatus[1]);
+    }
+
+    public int[] dfs1(TreeNode node) {
+        if (node == null) {
+            return new int[]{0, 0};
+        }
+        int[] l = dfs1(node.left);
+        int[] r = dfs1(node.right);
+        int selected = node.val + l[1] + r[1];
+        int notSelected = Math.max(l[0], l[1]) + Math.max(r[0], r[1]);
+        return new int[]{selected, notSelected};
+    }
+
 }
