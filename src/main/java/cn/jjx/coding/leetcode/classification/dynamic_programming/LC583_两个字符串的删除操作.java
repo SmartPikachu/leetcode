ff0eb18ea1086a667d94cc102cce4652e0c351d0
Pikachu <jiangjiaxin2011@126.com>
2022-04-17 17:11:32 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/dynamic_programming/LC583_两个字符串的删除操作.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/dynamic_programming/LC583_两个字符串的删除操作.java
@@ -2,6 +2,14 @@ package cn.jjx.coding.leetcode.classification.dynamic_programming;
 
 public class LC583_两个字符串的删除操作 {
 
+    /**
+     *为了使删除操作的次数最少，剩下的字符应尽可能多。
+     * 当剩下的字符为两个字符串的最长公共子序列时，删除操作的次数最少。
+     * 因此，可以计算两个字符串的最长公共子序列的长度，
+     * 然后分别计算两个字符串的长度和最长公共子序列的长度之差，
+     * 即为两个字符串分别需要删除的字符数，
+     * 两个字符串各自需要删除的字符数之和即为最少的删除操作的总次数。
+     */
     public int minDistance(String word1, String word2) {
         int m = word1.length(), n = word2.length();
         int[][] dp = new int[m + 1][n + 1];
@@ -21,6 +29,7 @@ public class LC583_两个字符串的删除操作 {
     }
 
 
+    //或者直接采用动态规划，dp[i][j]表示使word1[0:i]和word2[0:j]相同的最小删除次数。
     public int minDistance1(String word1, String word2) {
         int m = word1.length(), n = word2.length();
         int[][] dp = new int[m + 1][n + 1];
--- src/main/java/cn/jjx/coding/leetcode/classification/dynamic_programming/LC647_回文子串.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/dynamic_programming/LC647_回文子串.java
@@ -2,6 +2,7 @@ package cn.jjx.coding.leetcode.classification.dynamic_programming;
 
 public class LC647_回文子串 {
 
+    //中心扩展，要明白2n-1的由来，奇数和偶数的中心点不一样。
     public int countSubstrings(String s) {
         int n = s.length(), ans = 0;
         for (int i = 0; i < 2 * n - 1; ++i) {
@@ -15,34 +16,22 @@ public class LC647_回文子串 {
         return ans;
     }
 
-
+    //采用动态规划的方法
     public int countSubstrings1(String s) {
-        if(s == null || s.equals("")){
-            return 0;
-        }
-        int n = s.length();
-        boolean[][] dp = new boolean[n][n];
-        int result = s.length();
-        for(int i = 0; i<n; i++) dp[i][i] = true;
-        for(int i = n-1; i>=0; i--){
-            for(int j = i+1; j<n; j++){
-                if(s.charAt(i) == s.charAt(j)) {
-                    if(j-i == 1){
-                        dp[i][j] = true;
-                    }
-                    else{
-                        dp[i][j] = dp[i+1][j-1];
-                    }
-                }else{
-                    dp[i][j] = false;
-                }
-                if(dp[i][j]){
-                    result++;
+        // 动态规划法
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        int ans = 0;
+
+        for (int j = 0; j < s.length(); j++) {
+            for (int i = 0; i <= j; i++) {
+                if (s.charAt(i) == s.charAt(j) && (j - i < 2 || dp[i + 1][j - 1])) {
+                    dp[i][j] = true;
+                    ans++;
                 }
             }
         }
-        return result;
 
+        return ans;
     }
 
 }
