317ec6cb7f64ee6d371f0aa7086a23bf3997c4ff
Pikachu <jiangjiaxin2011@126.com>
2022-04-19 20:55:28 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/greedy/LC134_加油站.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/greedy/LC134_加油站.java
@@ -1,24 +1,10 @@
 package cn.jjx.coding.leetcode.classification.greedy;
 
 public class LC134_加油站 {
+
+    //官方方法，直接通过推到得出从，如果从x无法到达y,那么
+    // x,y 之间的任何一个加油站出发，都无法到达加油站 y 的下一个加油站。
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int curSum = 0;
-        int totalSum = 0;
-        int index = 0;
-        for (int i = 0; i < gas.length; i++) {
-            curSum += gas[i] - cost[i];
-            totalSum += gas[i] - cost[i];
-            if (curSum < 0) {
-                index = (i + 1) % gas.length ;
-                curSum = 0;
-            }
-        }
-        if (totalSum < 0) return -1;
-        return index;
-    }
-
-
-    public int canCompleteCircuit1(int[] gas, int[] cost) {
         int n = gas.length;
         int i = 0;
         while (i < n) {
@@ -40,6 +26,23 @@ public class LC134_加油站 {
             }
         }
         return -1;
+    }
+
+    //代码随想录方法
+    public int canCompleteCircuit1(int[] gas, int[] cost) {
+        int curSum = 0;
+        int totalSum = 0;
+        int index = 0;
+        for (int i = 0; i < gas.length; i++) {
+            curSum += gas[i] - cost[i];
+            totalSum += gas[i] - cost[i];
+            if (curSum < 0) {
+                index = (i + 1) % gas.length ;
+                curSum = 0;
+            }
+        }
+        if (totalSum < 0) return -1;
+        return index;
     }
 
 }
