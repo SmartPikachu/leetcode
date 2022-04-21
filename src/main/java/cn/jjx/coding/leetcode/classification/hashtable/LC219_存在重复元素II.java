f0139e2b813af2131e742085382716e7c0eb9390
Pikachu <jiangjiaxin2011@126.com>
2022-04-21 17:34:35 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/hashtable/LC219_存在重复元素II.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/hashtable/LC219_存在重复元素II.java
@@ -1,4 +1,38 @@
 package cn.jjx.coding.leetcode.classification.hashtable;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class LC219_存在重复元素II {
+
+    //使用哈希表
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer,Integer> map = new HashMap<>();
+        int length = nums.length;
+        for(int i=0;i<length;i++){
+            int num = nums[i];
+            if(map.containsKey(num) && i-map.get(num)<=k){
+                return true;
+            }
+            map.put(num,i);
+        }
+        return false;
+    }
+
+    //使用滑动窗口的方法
+    public boolean containsNearbyDuplicate1(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+        int length = nums.length;
+        for(int i=0;i<length;i++){
+            if(i>k){
+                set.remove(nums[i-k-1]);
+            }
+            if(!set.add(nums[i])){
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- src/main/java/cn/jjx/coding/leetcode/classification/hashtable/LC49_字母异位词分组.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/hashtable/LC49_字母异位词分组.java
@@ -1,4 +1,43 @@
 package cn.jjx.coding.leetcode.classification.hashtable;
 
+import java.util.*;
+
 public class LC49_字母异位词分组 {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String,List<String>> map  = new HashMap<>();
+        for(String str:strs){
+            int[] counts = new int[26];
+            int length = str.length();
+            for(int i=0;i<length;i++){
+                counts[str.charAt(i)-'a']++;
+            }
+            StringBuffer sb = new StringBuffer();
+            for(int i=0;i<26;i++){
+                if(counts[i]!=0){
+                    sb.append((char)('a'+i));
+                    sb.append(counts[i]);
+                }
+            }
+            String key = sb.toString();
+            List<String> list = map.getOrDefault(key,new ArrayList<String >());
+            list.add(str);
+            map.put(key,list);
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+
+    //直接把排序好的单词作为键，这样代码感觉会简洁
+    public List<List<String>> groupAnagrams1(String[] strs) {
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        for (String str : strs) {
+            char[] array = str.toCharArray();
+            Arrays.sort(array);
+            String key = new String(array);
+            List<String> list = map.getOrDefault(key, new ArrayList<String>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
 }
