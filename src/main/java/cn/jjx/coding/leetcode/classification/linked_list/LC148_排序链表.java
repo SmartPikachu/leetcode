e1a208315a19c0f55b3576367c95106999adf057
Pikachu <jiangjiaxin2011@126.com>
2022-04-24 01:53:56 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/linked_list/LC148_排序链表.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/linked_list/LC148_排序链表.java
@@ -1,4 +1,101 @@
 package cn.jjx.coding.leetcode.classification.linked_list;
 
+import cn.jjx.coding.leetcode.data_structure.ListNode;
+
+
+
 public class LC148_排序链表 {
+
+    //采用自顶向下的方法,代码比较容易实现。
+    public ListNode sortList(ListNode head) {
+        return sortList(head,null);
+    }
+
+    public ListNode sortList(ListNode head,ListNode tail){
+        if(head==null){
+            return head;
+        }
+        if(head.next==tail){
+            head.next=null;
+            return head;
+        }
+        ListNode slow=head,fast=head;
+        while(fast!=tail){
+            slow=slow.next;
+            fast=fast.next;
+            if(fast!=tail){
+                fast=fast.next;
+            }
+        }
+        ListNode mid = slow;
+        ListNode list1=sortList(head,mid);
+        ListNode list2=sortList(mid,tail);
+        ListNode sorted = merge(list1,list2);
+        return sorted;
+
+    }
+
+
+    public ListNode merge(ListNode head1,ListNode head2){
+        ListNode dummyHead = new ListNode(0);
+        ListNode temp = dummyHead,temp1=head1,temp2=head2;
+        while(temp1!=null && temp2!=null){
+            if(temp1.val<=temp2.val){
+                temp.next=temp1;
+                temp1=temp1.next;
+            }else{
+                temp.next=temp2;
+                temp2=temp2.next;
+            }
+            temp=temp.next;
+        }
+        if(temp1!=null){
+            temp.next=temp1;
+        }else if(temp2!=null){
+            temp.next=temp2;
+        }
+        return dummyHead.next;
+    }
+
+    //这种自底向上的方法不是很好写，但是思路很清晰。
+    public ListNode sortList1(ListNode head) {
+        if(head==null){
+            return head;
+        }
+        int length=0;
+        ListNode node = head;
+        while(node != null){
+            length++;
+            node=node.next;
+        }
+        ListNode dummyHead = new ListNode(0,head);
+        for(int subLength=1;subLength<length;subLength<<=1){
+            ListNode prev = dummyHead,curr=dummyHead.next;
+            while(curr!=null){
+                ListNode head1 = curr;
+                //如果链表的长度偶数走不到这里，如果是奇数这里也不许判断是否为空。
+                for(int i=1;i<subLength&&curr.next!=null;i++){
+                    curr=curr.next;
+                }
+                ListNode head2=curr.next;
+                curr.next=null;
+                curr=head2;
+                for(int i=1;i<subLength&&curr!=null&&curr.next!=null;i++){
+                    curr=curr.next;
+                }
+                ListNode next = null;
+                if(curr!=null){
+                    next=curr.next;
+                    curr.next=null;
+                }
+                ListNode merged = merge(head1,head2);
+                prev.next=merged;
+                while(prev.next!=null){
+                    prev=prev.next;
+                }
+                curr=next;
+            }
+        }
+        return dummyHead.next;
+    }
 }
