8351e956dd49b0b305045dd0b57e8ff47328cca2
Pikachu <jiangjiaxin2011@126.com>
2022-04-24 15:07:39 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/math_bit/LC29_两数相除.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/math_bit/LC29_两数相除.java
@@ -1,56 +1,58 @@
 package cn.jjx.coding.leetcode.classification.math_bit;
 
-public class LC29_两数相除 {public int divide(int dividend, int divisor) {
+public class LC29_两数相除 {
 
-    // 考虑被除数为最小值的情况
-    if (dividend == Integer.MIN_VALUE) {
-        if (divisor == 1) {
-            return Integer.MIN_VALUE;
-        }
-        if (divisor == -1) {
-            return Integer.MAX_VALUE;
-        }
-    }
-    // 考虑除数为最小值的情况
-    if (divisor == Integer.MIN_VALUE) {
-        return dividend == Integer.MIN_VALUE ? 1 : 0;
-    }
-    // 考虑被除数为 0 的情况
-    if (dividend == 0) {
-        return 0;
-    }
-
-    // 一般情况，使用二分查找
-    // 将所有的正数取相反数，这样就只需要考虑一种情况
-    boolean rev = false;
-    if (dividend > 0) {
-        dividend = -dividend;
-        rev = !rev;
-    }
-    if (divisor > 0) {
-        divisor = -divisor;
-        rev = !rev;
-    }
-
-    int left = 1, right = Integer.MAX_VALUE, ans = 0;
-    while (left <= right) {
-        // 注意溢出，并且不能使用除法
-        int mid = left + ((right - left) >> 1);
-        boolean check = quickAdd(divisor, mid, dividend);
-        if (check) {
-            ans = mid;
-            // 注意溢出
-            if (mid == Integer.MAX_VALUE) {
-                break;
+    //这个是官方的题解，还是有点疑问。
+    public int divide(int dividend, int divisor) {
+        // 考虑被除数为最小值的情况
+        if (dividend == Integer.MIN_VALUE) {
+            if (divisor == 1) {
+                return Integer.MIN_VALUE;
             }
-            left = mid + 1;
-        } else {
-            right = mid - 1;
+            if (divisor == -1) {
+                return Integer.MAX_VALUE;
+            }
         }
-    }
+        // 考虑除数为最小值的情况
+        if (divisor == Integer.MIN_VALUE) {
+            return dividend == Integer.MIN_VALUE ? 1 : 0;
+        }
+        // 考虑被除数为 0 的情况
+        if (dividend == 0) {
+            return 0;
+        }
 
-    return rev ? -ans : ans;
-}
+        // 一般情况，使用二分查找
+        // 将所有的正数取相反数，这样就只需要考虑一种情况
+        boolean rev = false;
+        if (dividend > 0) {
+            dividend = -dividend;
+            rev = !rev;
+        }
+        if (divisor > 0) {
+            divisor = -divisor;
+            rev = !rev;
+        }
+
+        int left = 1, right = Integer.MAX_VALUE, ans = 0;
+        while (left <= right) {
+            // 注意溢出，并且不能使用除法
+            int mid = left + ((right - left) >> 1);
+            boolean check = quickAdd(divisor, mid, dividend);
+            if (check) {
+                ans = mid;
+                // 注意溢出
+                if (mid == Integer.MAX_VALUE) {
+                    break;
+                }
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return rev ? -ans : ans;
+    }
 
     // 快速乘
     public boolean quickAdd(int y, int z, int x) {
@@ -77,6 +79,5 @@ public class LC29_两数相除 {public int divide(int dividend, int divisor) {
         }
         return true;
     }
-
 
 }
