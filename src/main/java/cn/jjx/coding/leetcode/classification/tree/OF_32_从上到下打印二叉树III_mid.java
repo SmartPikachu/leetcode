dcb490003e09e76d204d4f8c2b8d643cab5cd7a0
Pikachu <jiangjiaxin2011@126.com>
2022-05-05 22:44:51 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/tree/OF_32_从上到下打印二叉树III_mid.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/tree/OF_32_从上到下打印二叉树III_mid.java
@@ -1,4 +1,37 @@
 package cn.jjx.coding.leetcode.classification.tree;
 
+import cn.jjx.coding.leetcode.data_structure.TreeNode;
+
+import java.util.*;
+
 public class OF_32_从上到下打印二叉树III_mid {
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans = new LinkedList<>();
+        if(root==null) return ans;
+        Queue<TreeNode> nodeQueue = new ArrayDeque<>();
+        nodeQueue.offer(root);
+        boolean isOrderLeft=true;
+        while(!nodeQueue.isEmpty()){
+            Deque<Integer> levelList = new LinkedList<>();
+            int size = nodeQueue.size();
+            for(int i=0;i<size;i++){
+                TreeNode curNode = nodeQueue.poll();
+                if(isOrderLeft){
+                    levelList.offerLast(curNode.val);
+                }else{
+                    levelList.offerFirst(curNode.val);
+                }
+                if(curNode.left!=null){
+                    nodeQueue.offer(curNode.left);
+                }
+                if(curNode.right!=null){
+                    nodeQueue.offer(curNode.right);
+                }
+            }
+            ans.add(new LinkedList<Integer>(levelList));
+            isOrderLeft=!isOrderLeft;
+        }
+        return ans;
+    }
 }
--- src/main/java/cn/jjx/coding/leetcode/classification/tree/OF_34_二叉树中和为某一值的路径_mid.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/tree/OF_34_二叉树中和为某一值的路径_mid.java
@@ -1,4 +1,5 @@
 package cn.jjx.coding.leetcode.classification.tree;
 
 public class OF_34_二叉树中和为某一值的路径_mid {
+
 }
