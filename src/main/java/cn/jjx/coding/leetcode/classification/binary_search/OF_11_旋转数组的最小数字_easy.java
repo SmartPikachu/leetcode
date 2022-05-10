62bfe89a38c5ca0ce731cfad7fc635523e7dc595
Pikachu <jiangjiaxin2011@126.com>
2022-05-10 23:07:43 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/binary_search/OF_11_旋转数组的最小数字_easy.java
+++
@@ -1,4 +0,0 @@
-package cn.jjx.coding.leetcode.classification.binary_search;
-
-public class OF_11_旋转数组的最小数字_easy {
-}
--- src/main/java/cn/jjx/coding/leetcode/classification/binary_search/OF_53_II_0到n_1中缺失的数字_easy.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/binary_search/OF_53_II_0到n_1中缺失的数字_easy.java
@@ -1,4 +1,13 @@
 package cn.jjx.coding.leetcode.classification.binary_search;
 
 public class OF_53_II_0到n_1中缺失的数字_easy {
+    public int missingNumber(int[] nums) {
+        int i = 0, j = nums.length - 1;
+        while(i <= j) {
+            int m = (i + j) / 2;
+            if(nums[m] == m) i = m + 1;
+            else j = m - 1;
+        }
+        return i;
+    }
 }
