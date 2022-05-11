f7ec38802fc475aadf4e8263218070bc07adbc1c
Pikachu <jiangjiaxin2011@126.com>
2022-05-11 23:53:57 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/dfs_bfs/LC128_最长连续序列_mid.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/dfs_bfs/LC128_最长连续序列_mid.java
@@ -1,4 +1,32 @@
 package cn.jjx.coding.leetcode.classification.dfs_bfs;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class LC128_最长连续序列_mid {
+
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> num_set = new HashSet<Integer>();
+        for (int num : nums) {
+            num_set.add(num);
+        }
+
+        int longestStreak = 0;
+
+        for (int num : num_set) {
+            if (!num_set.contains(num - 1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (num_set.contains(currentNum + 1)) {
+                    currentNum += 1;
+                    currentStreak += 1;
+                }
+
+                longestStreak = Math.max(longestStreak, currentStreak);
+            }
+        }
+
+        return longestStreak;
+    }
 }
