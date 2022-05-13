993494bd6cdca7b1f0b963a0b8189989877bcf66
Pikachu <jiangjiaxin2011@126.com>
2022-05-13 09:37:33 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/dynamic_programming/OF_47_礼物的最大价值_mid.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/dynamic_programming/OF_47_礼物的最大价值_mid.java
@@ -1,4 +1,18 @@
 package cn.jjx.coding.leetcode.classification.dynamic_programming;
 
 public class OF_47_礼物的最大价值_mid {
+
+    public int maxValue(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i == 0 && j == 0) continue;
+                if(i == 0) grid[i][j] += grid[i][j - 1] ;
+                else if(j == 0) grid[i][j] += grid[i - 1][j];
+                else grid[i][j] += Math.max(grid[i][j - 1], grid[i - 1][j]);
+            }
+        }
+        return grid[m - 1][n - 1];
+    }
+
 }
--- src/main/java/cn/jjx/coding/leetcode/classification/dynamic_programming/OF_60_n个骰子的点数_mid.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/dynamic_programming/OF_60_n个骰子的点数_mid.java
@@ -1,4 +1,22 @@
 package cn.jjx.coding.leetcode.classification.dynamic_programming;
 
 public class OF_60_n个骰子的点数_mid {
+
+    public double[] dicesProbability(int n) {
+
+        int[][] dp = new int[n + 1][6 * n + 1];
+        for(int i = 1; i <= 6; i++)
+            dp[1][i] = 1;
+
+        for(int i = 2; i <= n; i++)
+            for(int j = i; j <= 6 * i; j++)
+                for(int k = 1; k <= 6 && k <= j; k++)
+                    dp[i][j] += dp[i-1][j - k];
+
+        double[] ans = new double[5*n + 1];
+        for(int i = n; i <= 6 * n; i++)
+            ans[i - n] = ((double)dp[n][i]) / (Math.pow(6,n));
+        return ans;
+
+    }
 }
