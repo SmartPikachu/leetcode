d7a79b8f9325effec0a1d8071b745aa64e1c733e
Pikachu <jiangjiaxin2011@126.com>
2022-05-16 00:09:20 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/array/LC6_Z字形变换_mid.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/array/LC6_Z字形变换_mid.java
@@ -1,4 +1,21 @@
 package cn.jjx.coding.leetcode.classification.array;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LC6_Z字形变换_mid {
+    public String convert(String s, int numRows) {
+        if(numRows<2) return s;
+        List<StringBuilder> rows = new ArrayList<>();
+        for(int i=0;i<numRows;i++) rows.add(new StringBuilder());
+        int i=0,flag=-1;
+        for(char c:s.toCharArray()){
+            rows.get(i).append(c);
+            if(i==0 || i==numRows-1) flag=-flag;
+            i+=flag;
+        }
+        StringBuilder res = new StringBuilder();
+        for(StringBuilder row:rows) res.append(row);
+        return res.toString();
+    }
 }
