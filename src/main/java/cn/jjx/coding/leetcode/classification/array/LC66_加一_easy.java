225019ccd84777333aff795b7fc49ea5372f9306
Pikachu <jiangjiaxin2011@126.com>
2022-05-16 22:33:58 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/array/LC66_加一_easy.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/array/LC66_加一_easy.java
@@ -1,4 +1,22 @@
 package cn.jjx.coding.leetcode.classification.array;
 
 public class LC66_加一_easy {
+
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        for (int i = n - 1; i >= 0; --i) {
+            if (digits[i] != 9) {
+                ++digits[i];
+                for (int j = i + 1; j < n; ++j) {
+                    digits[j] = 0;
+                }
+                return digits;
+            }
+        }
+
+        // digits 中所有的元素均为 9
+        int[] ans = new int[n + 1];
+        ans[0] = 1;
+        return ans;
+    }
 }
