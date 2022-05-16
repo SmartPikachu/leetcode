2ac8f86a4990cc2dd24eed45a3ae2125fd0f5ece
Pikachu <jiangjiaxin2011@126.com>
2022-05-16 23:24:03 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/array/LC118_杨辉三角_easy.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/array/LC118_杨辉三角_easy.java
@@ -1,4 +1,23 @@
 package cn.jjx.coding.leetcode.classification.array;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LC118_杨辉三角_easy {
+
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ret = new ArrayList<List<Integer>>();
+        for (int i = 0; i < numRows; ++i) {
+            List<Integer> row = new ArrayList<Integer>();
+            for (int j = 0; j <= i; ++j) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    row.add(ret.get(i - 1).get(j - 1) + ret.get(i - 1).get(j));
+                }
+            }
+            ret.add(row);
+        }
+        return ret;
+    }
 }
--- src/main/java/cn/jjx/coding/leetcode/classification/array/LC119_杨辉三角II_easy.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/array/LC119_杨辉三角II_easy.java
@@ -1,4 +1,23 @@
 package cn.jjx.coding.leetcode.classification.array;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LC119_杨辉三角II_easy {
+
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> pre = new ArrayList<Integer>();
+        for (int i = 0; i <= rowIndex; ++i) {
+            List<Integer> cur = new ArrayList<Integer>();
+            for (int j = 0; j <= i; ++j) {
+                if (j == 0 || j == i) {
+                    cur.add(1);
+                } else {
+                    cur.add(pre.get(j - 1) + pre.get(j));
+                }
+            }
+            pre = cur;
+        }
+        return pre;
+    }
 }
