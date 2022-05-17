5e6f0578622c994216a0ca7b502cfbebb371ee1a
Pikachu <jiangjiaxin2011@126.com>
2022-05-18 01:40:27 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/array/LC268_丢弃的数字_easy.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/array/LC268_丢弃的数字_easy.java
@@ -1,4 +1,36 @@
 package cn.jjx.coding.leetcode.classification.array;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class LC268_丢弃的数字_easy {
+    //位运算
+    public int missingNumber(int[] nums) {
+        int xor = 0;
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            xor ^= nums[i];
+        }
+        for (int i = 0; i <= n; i++) {
+            xor ^= i;
+        }
+        return xor;
+    }
+
+
+    public int missingNumber1(int[] nums) {
+        Set<Integer> set = new HashSet<Integer>();
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            set.add(nums[i]);
+        }
+        int missing = -1;
+        for (int i = 0; i <= n; i++) {
+            if (!set.contains(i)) {
+                missing = i;
+                break;
+            }
+        }
+        return missing;
+    }
 }
--- src/main/java/cn/jjx/coding/leetcode/classification/array/LC277_搜寻名人_mid.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/array/LC277_搜寻名人_mid.java
@@ -1,4 +1,30 @@
 package cn.jjx.coding.leetcode.classification.array;
 
 public class LC277_搜寻名人_mid {
+
+    public int findCelebrity(int n) {
+        int ans = 0;
+        for(int k = 0; k < n; k++){
+            if(knows(ans, k)){
+                ans = k;
+            }
+        }
+
+        for(int k = 0; k < n; k++){
+            if(k != ans){
+                if(!knows(k, ans)){
+                    return -1;
+                }
+                if(knows(ans, k)){
+                    return -1;
+                }
+            }
+        }
+        return ans;
+    }
+
+    public boolean knows(int i,int j){
+        //没有实现
+        return true;
+    }
 }
