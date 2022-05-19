54781c228ba60dcdbc4fbdfa570527004f6c92dc
Pikachu <jiangjiaxin2011@126.com>
2022-05-19 23:05:59 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/array/LC406_根据身高重建队列_mid.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/array/LC406_根据身高重建队列_mid.java
@@ -1,4 +1,57 @@
 package cn.jjx.coding.leetcode.classification.array;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public class LC406_根据身高重建队列_mid {
+
+    //从高到低考虑
+    public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, new Comparator<int[]>() {
+            public int compare(int[] person1, int[] person2) {
+                if (person1[0] != person2[0]) {
+                    return person2[0] - person1[0];
+                } else {
+                    return person1[1] - person2[1];
+                }
+            }
+        });
+        List<int[]> ans = new ArrayList<int[]>();
+        for (int[] person : people) {
+            ans.add(person[1], person);
+        }
+        return ans.toArray(new int[ans.size()][]);
+    }
+
+
+    //从低到高考虑
+    public int[][] reconstructQueue1(int[][] people) {
+        Arrays.sort(people, new Comparator<int[]>() {
+            public int compare(int[] person1, int[] person2) {
+                if (person1[0] != person2[0]) {
+                    return person1[0] - person2[0];
+                } else {
+                    return person2[1] - person1[1];
+                }
+            }
+        });
+        int n = people.length;
+        int[][] ans = new int[n][];
+        for (int[] person : people) {
+            int spaces = person[1] + 1;
+            for (int i = 0; i < n; ++i) {
+                if (ans[i] == null) {
+                    --spaces;
+                    if (spaces == 0) {
+                        ans[i] = person;
+                        break;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+
 }
--- src/main/java/cn/jjx/coding/leetcode/classification/array/LC498_对角线遍历_mid.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/array/LC498_对角线遍历_mid.java
@@ -1,4 +1,33 @@
 package cn.jjx.coding.leetcode.classification.array;
 
 public class LC498_对角线遍历_mid {
+
+    //本题并没有选择官方题解
+    public int[] findDiagonalOrder(int[][] matrix) {
+        if (matrix == null || matrix.length == 0) return new int[0];
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[] nums = new int[m * n];
+
+        int k = 0;
+        boolean bXFlag = true;
+        for (int i = 0; i < m+n-1; i++){
+            int pm = bXFlag ? m : n;
+            int pn = bXFlag ? n : m;
+
+            int x = (i < pm) ? i : pm - 1;
+            int y = i - x;
+
+            while (x >= 0 && y < pn){
+                nums[k++] = bXFlag ? matrix[x][y] : matrix[y][x];
+                x--;
+                y++;
+            }
+
+            bXFlag = !bXFlag;
+        }
+
+        return nums;
+    }
 }
