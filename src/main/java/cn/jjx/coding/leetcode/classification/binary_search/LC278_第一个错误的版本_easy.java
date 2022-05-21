42277e790515514396230402cd1cdd883a9e9e80
Pikachu <jiangjiaxin2011@126.com>
2022-05-21 23:31:14 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/binary_search/LC278_第一个错误的版本_easy.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/binary_search/LC278_第一个错误的版本_easy.java
@@ -1,4 +1,23 @@
 package cn.jjx.coding.leetcode.classification.binary_search;
 
 public class LC278_第一个错误的版本_easy {
+
+    public int firstBadVersion(int n) {
+        int left = 1, right = n;
+        while (left < right) { // 循环直至区间左右端点相同
+            int mid = left + (right - left) / 2; // 防止计算时溢出
+            if (isBadVersion(mid)) {
+                right = mid; // 答案在区间 [left, mid] 中
+            } else {
+                left = mid + 1; // 答案在区间 [mid+1, right] 中
+            }
+        }
+        // 此时有 left == right，区间缩为一个点，即为答案
+        return left;
+    }
+
+    //指定的API并没有给实现的方法
+    boolean isBadVersion(int version){
+        return true;
+    }
 }
--- src/main/java/cn/jjx/coding/leetcode/classification/binary_search/LC35_搜索插入位置_easy.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/binary_search/LC35_搜索插入位置_easy.java
@@ -1,4 +1,18 @@
 package cn.jjx.coding.leetcode.classification.binary_search;
 
 public class LC35_搜索插入位置_easy {
+    public int searchInsert(int[] nums, int target) {
+        int n = nums.length;
+        int left = 0, right = n - 1, ans = n;
+        while (left <= right) {
+            int mid = ((right - left) >> 1) + left;
+            if (target <= nums[mid]) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
 }
