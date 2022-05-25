6d9b7e5f0676bce91f3435aaf8534121c7b8d009
Pikachu <jiangjiaxin2011@126.com>
2022-05-25 12:20:16 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/dfs_bfs/LC695_岛屿的最大面积_hard.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/dfs_bfs/LC695_岛屿的最大面积_hard.java
@@ -1,4 +1,68 @@
 package cn.jjx.coding.leetcode.classification.dfs_bfs;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class LC695_岛屿的最大面积_hard {
+
+    //深度优先搜索
+    public int maxAreaOfIsland(int[][] grid) {
+        int ans = 0;
+        for (int i = 0; i != grid.length; ++i) {
+            for (int j = 0; j != grid[0].length; ++j) {
+                ans = Math.max(ans, dfs(grid, i, j));
+            }
+        }
+        return ans;
+    }
+
+    public int dfs(int[][] grid, int cur_i, int cur_j) {
+        if (cur_i < 0 || cur_j < 0 || cur_i == grid.length ||
+         cur_j == grid[0].length || grid[cur_i][cur_j] != 1) {
+            return 0;
+        }
+        grid[cur_i][cur_j] = 0;
+        int[] di = {0, 0, 1, -1};
+        int[] dj = {1, -1, 0, 0};
+        int ans = 1;
+        for (int index = 0; index != 4; ++index) {
+            int next_i = cur_i + di[index], next_j = cur_j + dj[index];
+            ans += dfs(grid, next_i, next_j);
+        }
+        return ans;
+    }
+
+
+    //广度优先搜索
+    public int maxAreaOfIsland1(int[][] grid) {
+        int ans = 0;
+        for (int i = 0; i != grid.length; ++i) {
+            for (int j = 0; j != grid[0].length; ++j) {
+                int cur = 0;
+                Queue<Integer> queuei = new LinkedList<Integer>();
+                Queue<Integer> queuej = new LinkedList<Integer>();
+                queuei.offer(i);
+                queuej.offer(j);
+                while (!queuei.isEmpty()) {
+                    int cur_i = queuei.poll(), cur_j = queuej.poll();
+                    if (cur_i < 0 || cur_j < 0 || cur_i == grid.length
+                    || cur_j == grid[0].length || grid[cur_i][cur_j] != 1) {
+                        continue;
+                    }
+                    ++cur;
+                    grid[cur_i][cur_j] = 0;
+                    int[] di = {0, 0, 1, -1};
+                    int[] dj = {1, -1, 0, 0};
+                    for (int index = 0; index != 4; ++index) {
+                        int next_i = cur_i + di[index], next_j = cur_j + dj[index];
+                        queuei.offer(next_i);
+                        queuej.offer(next_j);
+                    }
+                }
+                ans = Math.max(ans, cur);
+            }
+        }
+        return ans;
+    }
+
 }
--- src/main/java/cn/jjx/coding/leetcode/classification/dfs_bfs/LC838_推多米诺_mid.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/dfs_bfs/LC838_推多米诺_mid.java
@@ -1,4 +1,77 @@
 package cn.jjx.coding.leetcode.classification.dfs_bfs;
 
+import java.util.*;
+
 public class LC838_推多米诺_mid {
+
+    //采用模拟的方法，代码简单，但是很难想出设置虚拟节点的方法
+    public String pushDominoes(String dominoes) {
+        char[] s = dominoes.toCharArray();
+        int n = s.length, i = 0;
+        char left = 'L';
+        while (i < n) {
+            int j = i;
+            while (j < n && s[j] == '.') { // 找到一段连续的没有被推动的骨牌
+                j++;
+            }
+            char right = j < n ? s[j] : 'R';
+            if (left == right) { // 方向相同，那么这些竖立骨牌也会倒向同一方向
+                while (i < j) {
+                    s[i++] = right;
+                }
+            } else if (left == 'R' && right == 'L') { // 方向相对，那么就从两侧向中间倒
+                int k = j - 1;
+                while (i < k) {
+                    s[i++] = 'R';
+                    s[k--] = 'L';
+                }
+            }
+            left = right;
+            i = j + 1;
+        }
+        return new String(s);
+
+    }
+
+    //广度优先搜索，也不是很容易想出这种解决方案
+    public String pushDominoes1(String dominoes) {
+        int n = dominoes.length();
+        Deque<Integer> queue = new ArrayDeque<Integer>();
+        int[] time = new int[n];
+        Arrays.fill(time, -1);
+        List<Character>[] force = new List[n];
+        for (int i = 0; i < n; i++) {
+            force[i] = new ArrayList<Character>();
+        }
+        for (int i = 0; i < n; i++) {
+            char f = dominoes.charAt(i);
+            if (f != '.') {
+                queue.offer(i);
+                time[i] = 0;
+                force[i].add(f);
+            }
+        }
+
+        char[] res = new char[n];
+        Arrays.fill(res, '.');
+        while (!queue.isEmpty()) {
+            int i = queue.poll();
+            if (force[i].size() == 1) {
+                char f = force[i].get(0);
+                res[i] = f;
+                int ni = f == 'L' ? i - 1 : i + 1;
+                if (ni >= 0 && ni < n) {
+                    int t = time[i];
+                    if (time[ni] == -1) {
+                        queue.offer(ni);
+                        time[ni] = t + 1;
+                        force[ni].add(f);
+                    } else if (time[ni] == t + 1) {
+                        force[ni].add(f);
+                    }
+                }
+            }
+        }
+        return new String(res);
+    }
 }
