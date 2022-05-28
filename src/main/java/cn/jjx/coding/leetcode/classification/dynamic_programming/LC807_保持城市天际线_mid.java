252a2666ae4b43412063d46f838868fdee193d03
Pikachu <jiangjiaxin2011@126.com>
2022-05-28 23:20:04 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/dynamic_programming/LC807_保持城市天际线_mid.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/dynamic_programming/LC807_保持城市天际线_mid.java
@@ -1,4 +1,22 @@
 package cn.jjx.coding.leetcode.classification.dynamic_programming;
 
 public class LC807_保持城市天际线_mid {
+    public int maxIncreaseKeepingSkyline(int[][] grid) {
+        int n = grid.length;
+        int[] rowMax = new int[n];
+        int[] colMax = new int[n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                rowMax[i] = Math.max(rowMax[i], grid[i][j]);
+                colMax[j] = Math.max(colMax[j], grid[i][j]);
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                ans += Math.min(rowMax[i], colMax[j]) - grid[i][j];
+            }
+        }
+        return ans;
+    }
 }
