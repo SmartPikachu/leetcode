c350f2c14379691bb49057389528668aa5c1dfe5
Pikachu <jiangjiaxin2011@126.com>
2022-05-29 22:51:19 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/graph/拓扑排序/LC210_课程表II_mid.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/graph/拓扑排序/LC210_课程表II_mid.java
@@ -6,6 +6,61 @@ import java.util.List;
 import java.util.Queue;
 
 public class LC210_课程表II_mid {
+
+    //拓扑排序的方法，拓扑排序的本身就是有向无环图。
+    class Solution1 {
+        // 存储有向图
+        List<List<Integer>> edges;
+        // 存储每个节点的入度
+        int[] indeg;
+        // 存储答案
+        int[] result;
+        // 答案下标
+        int index;
+
+        public int[] findOrder(int numCourses, int[][] prerequisites) {
+            edges = new ArrayList<List<Integer>>();
+            for (int i = 0; i < numCourses; ++i) {
+                edges.add(new ArrayList<Integer>());
+            }
+            indeg = new int[numCourses];
+            result = new int[numCourses];
+            index = 0;
+            for (int[] info : prerequisites) {
+                edges.get(info[1]).add(info[0]);
+                ++indeg[info[0]];
+            }
+
+            Queue<Integer> queue = new LinkedList<Integer>();
+            // 将所有入度为 0 的节点放入队列中
+            for (int i = 0; i < numCourses; ++i) {
+                if (indeg[i] == 0) {
+                    queue.offer(i);
+                }
+            }
+
+            while (!queue.isEmpty()) {
+                // 从队首取出一个节点
+                int u = queue.poll();
+                // 放入答案中
+                result[index++] = u;
+                for (int v: edges.get(u)) {
+                    --indeg[v];
+                    // 如果相邻节点 v 的入度为 0，就可以选 v 对应的课程了
+                    if (indeg[v] == 0) {
+                        queue.offer(v);
+                    }
+                }
+            }
+
+            if (index != numCourses) {
+                return new int[0];
+            }
+            return result;
+        }
+    }
+
+
     //深度优先搜索
     class Solution {
         // 存储有向图
@@ -14,7 +69,7 @@ public class LC210_课程表II_mid {
         int[] visited;
         // 用数组来模拟栈，下标 n-1 为栈底，0 为栈顶
         int[] result;
-        // 判断有向图中是否有环
+        // 判断有向图中是否有环，true代表无环图。
         boolean valid = true;
         // 栈下标
         int index;
@@ -70,57 +125,7 @@ public class LC210_课程表II_mid {
     }
 
 
-    class Solution1 {
-        // 存储有向图
-        List<List<Integer>> edges;
-        // 存储每个节点的入度
-        int[] indeg;
-        // 存储答案
-        int[] result;
-        // 答案下标
-        int index;
 
-        public int[] findOrder(int numCourses, int[][] prerequisites) {
-            edges = new ArrayList<List<Integer>>();
-            for (int i = 0; i < numCourses; ++i) {
-                edges.add(new ArrayList<Integer>());
-            }
-            indeg = new int[numCourses];
-            result = new int[numCourses];
-            index = 0;
-            for (int[] info : prerequisites) {
-                edges.get(info[1]).add(info[0]);
-                ++indeg[info[0]];
-            }
-
-            Queue<Integer> queue = new LinkedList<Integer>();
-            // 将所有入度为 0 的节点放入队列中
-            for (int i = 0; i < numCourses; ++i) {
-                if (indeg[i] == 0) {
-                    queue.offer(i);
-                }
-            }
-
-            while (!queue.isEmpty()) {
-                // 从队首取出一个节点
-                int u = queue.poll();
-                // 放入答案中
-                result[index++] = u;
-                for (int v: edges.get(u)) {
-                    --indeg[v];
-                    // 如果相邻节点 v 的入度为 0，就可以选 v 对应的课程了
-                    if (indeg[v] == 0) {
-                        queue.offer(v);
-                    }
-                }
-            }
-
-            if (index != numCourses) {
-                return new int[0];
-            }
-            return result;
-        }
-    }
 
 
 }
--- src/main/java/cn/jjx/coding/leetcode/classification/graph/拓扑排序/LC269_火星词典.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/graph/拓扑排序/LC269_火星词典.java
@@ -1,4 +1,142 @@
 package cn.jjx.coding.leetcode.classification.graph.拓扑排序;
 
+import java.util.*;
+
 public class LC269_火星词典 {
+
+    //拓扑排序+广度优先搜索
+    class Solution {
+        Map<Character, List<Character>> edges = new HashMap<Character, List<Character>>();
+        Map<Character, Integer> indegrees = new HashMap<Character, Integer>();
+        boolean valid = true;
+
+        public String alienOrder(String[] words) {
+            int length = words.length;
+            for (String word : words) {
+                int wordLength = word.length();
+                for (int j = 0; j < wordLength; j++) {
+                    char c = word.charAt(j);
+                    edges.putIfAbsent(c, new ArrayList<Character>());
+                }
+            }
+            for (int i = 1; i < length && valid; i++) {
+                addEdge(words[i - 1], words[i]);
+            }
+            if (!valid) {
+                return "";
+            }
+            Queue<Character> queue = new ArrayDeque<Character>();
+            Set<Character> letterSet = edges.keySet();
+            for (char u : letterSet) {
+                if (!indegrees.containsKey(u)) {
+                    queue.offer(u);
+                }
+            }
+            StringBuffer order = new StringBuffer();
+            while (!queue.isEmpty()) {
+                char u = queue.poll();
+                order.append(u);
+                List<Character> adjacent = edges.get(u);
+                for (char v : adjacent) {
+                    indegrees.put(v, indegrees.get(v) - 1);
+                    if (indegrees.get(v) == 0) {
+                        queue.offer(v);
+                    }
+                }
+            }
+            return order.length() == edges.size() ? order.toString() : "";
+        }
+
+        public void addEdge(String before, String after) {
+            int length1 = before.length(), length2 = after.length();
+            int length = Math.min(length1, length2);
+            int index = 0;
+            while (index < length) {
+                char c1 = before.charAt(index), c2 = after.charAt(index);
+                if (c1 != c2) {
+                    edges.get(c1).add(c2);
+                    indegrees.put(c2, indegrees.getOrDefault(c2, 0) + 1);
+                    break;
+                }
+                index++;
+            }
+            if (index == length && length1 > length2) {
+                valid = false;
+            }
+        }
+    }
+
+
+    class Solution1 {
+        static final int VISITING = 1, VISITED = 2;
+        Map<Character, List<Character>> edges = new HashMap<Character, List<Character>>();
+        Map<Character, Integer> states = new HashMap<Character, Integer>();
+        boolean valid = true;
+        char[] order;
+        int index;
+
+        public String alienOrder(String[] words) {
+            int length = words.length;
+            for (String word : words) {
+                int wordLength = word.length();
+                for (int j = 0; j < wordLength; j++) {
+                    char c = word.charAt(j);
+                    edges.putIfAbsent(c, new ArrayList<Character>());
+                }
+            }
+            for (int i = 1; i < length && valid; i++) {
+                addEdge(words[i - 1], words[i]);
+            }
+            order = new char[edges.size()];
+            index = edges.size() - 1;
+            Set<Character> letterSet = edges.keySet();
+            for (char u : letterSet) {
+                if (!states.containsKey(u)) {
+                    dfs(u);
+                }
+            }
+            if (!valid) {
+                return "";
+            }
+            return new String(order);
+        }
+
+        public void addEdge(String before, String after) {
+            int length1 = before.length(), length2 = after.length();
+            int length = Math.min(length1, length2);
+            int index = 0;
+            while (index < length) {
+                char c1 = before.charAt(index), c2 = after.charAt(index);
+                if (c1 != c2) {
+                    edges.get(c1).add(c2);
+                    break;
+                }
+                index++;
+            }
+            if (index == length && length1 > length2) {
+                valid = false;
+            }
+        }
+
+        public void dfs(char u) {
+            states.put(u, VISITING);
+            List<Character> adjacent = edges.get(u);
+            for (char v : adjacent) {
+                if (!states.containsKey(v)) {
+                    dfs(v);
+                    if (!valid) {
+                        return;
+                    }
+                } else if (states.get(v) == VISITING) {
+                    valid = false;
+                    return;
+                }
+            }
+            states.put(u, VISITED);
+            order[index] = u;
+            index--;
+        }
+    }
+
+
 }
