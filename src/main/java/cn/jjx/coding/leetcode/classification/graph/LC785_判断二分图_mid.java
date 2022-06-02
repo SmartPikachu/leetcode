2bf9f55e8c918a2d4aa4234fba9589d4c6c329df
Pikachu <jiangjiaxin2011@126.com>
2022-06-02 23:43:56 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/graph/LC785_判断二分图_mid.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/graph/LC785_判断二分图_mid.java
@@ -1,6 +1,8 @@
 package cn.jjx.coding.leetcode.classification.graph;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class LC785_判断二分图_mid {
 
@@ -38,6 +40,39 @@ public class LC785_判断二分图_mid {
                     return;
                 }
             }
+        }
+    }
+
+    class Solution1 {
+        private static final int UNCOLORED = 0;
+        private static final int RED = 1;
+        private static final int GREEN = 2;
+        private int[] color;
+
+        public boolean isBipartite(int[][] graph) {
+            int n = graph.length;
+            color = new int[n];
+            Arrays.fill(color, UNCOLORED);
+            for (int i = 0; i < n; ++i) {
+                if (color[i] == UNCOLORED) {
+                    Queue<Integer> queue = new LinkedList<Integer>();
+                    queue.offer(i);
+                    color[i] = RED;
+                    while (!queue.isEmpty()) {
+                        int node = queue.poll();
+                        int cNei = color[node] == RED ? GREEN : RED;
+                        for (int neighbor : graph[node]) {
+                            if (color[neighbor] == UNCOLORED) {
+                                queue.offer(neighbor);
+                                color[neighbor] = cNei;
+                            } else if (color[neighbor] != cNei) {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+            return true;
         }
     }
 
