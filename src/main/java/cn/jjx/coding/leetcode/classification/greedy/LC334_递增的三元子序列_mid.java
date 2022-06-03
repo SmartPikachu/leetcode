9133a7e56f669234d847c152fe8308cd3c0338d5
Pikachu <jiangjiaxin2011@126.com>
2022-06-03 23:59:33 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/greedy/LC334_递增的三元子序列_mid.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/greedy/LC334_递增的三元子序列_mid.java
@@ -1,4 +1,49 @@
 package cn.jjx.coding.leetcode.classification.greedy;
 
 public class LC334_递增的三元子序列_mid {
+
+    //贪心算法
+    public boolean increasingTriplet(int[] nums) {
+        int n = nums.length;
+        if (n < 3) {
+            return false;
+        }
+        int first = nums[0], second = Integer.MAX_VALUE;
+        for (int i = 1; i < n; i++) {
+            int num = nums[i];
+            if (num > second) {
+                return true;
+            } else if (num > first) {
+                second = num;
+            } else {
+                first = num;
+            }
+        }
+        return false;
+    }
+
+
+    //双向遍历
+    public boolean increasingTriplet1(int[] nums) {
+        int n = nums.length;
+        if (n < 3) {
+            return false;
+        }
+        int[] leftMin = new int[n];
+        leftMin[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            leftMin[i] = Math.min(leftMin[i - 1], nums[i]);
+        }
+        int[] rightMax = new int[n];
+        rightMax[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], nums[i]);
+        }
+        for (int i = 1; i < n - 1; i++) {
+            if (nums[i] > leftMin[i - 1] && nums[i] < rightMax[i + 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
