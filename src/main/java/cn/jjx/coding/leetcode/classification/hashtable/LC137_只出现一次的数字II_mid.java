548fa0ca9f3db8af4950f57bc1e266b8c15c65b7
Pikachu <jiangjiaxin2011@126.com>
2022-06-05 21:06:17 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/hashtable/LC137_只出现一次的数字II_mid.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/hashtable/LC137_只出现一次的数字II_mid.java
@@ -1,4 +1,39 @@
 package cn.jjx.coding.leetcode.classification.hashtable;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LC137_只出现一次的数字II_mid {
+
+    public int singleNumber(int[] nums) {
+        Map<Integer, Integer> freq = new HashMap<Integer, Integer>();
+        for (int num : nums) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+        int ans = 0;
+        for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
+            int num = entry.getKey(), occ = entry.getValue();
+            if (occ == 1) {
+                ans = num;
+                break;
+            }
+        }
+        return ans;
+    }
+
+
+    public int singleNumber1(int[] nums) {
+        int ans = 0;
+        for (int i = 0; i < 32; ++i) {
+            int total = 0;
+            for (int num: nums) {
+                total += ((num >> i) & 1);
+            }
+            if (total % 3 != 0) {
+                ans |= (1 << i);
+            }
+        }
+        return ans;
+    }
+
 }
--- src/main/java/cn/jjx/coding/leetcode/classification/hashtable/LC169_多数元素_easy.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/hashtable/LC169_多数元素_easy.java
@@ -1,4 +1,41 @@
 package cn.jjx.coding.leetcode.classification.hashtable;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class LC169_多数元素_easy {
+
+    //哈希表
+    private Map<Integer, Integer> countNums(int[] nums) {
+        Map<Integer, Integer> counts = new HashMap<Integer, Integer>();
+        for (int num : nums) {
+            if (!counts.containsKey(num)) {
+                counts.put(num, 1);
+            } else {
+                counts.put(num, counts.get(num) + 1);
+            }
+        }
+        return counts;
+    }
+
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> counts = countNums(nums);
+
+        Map.Entry<Integer, Integer> majorityEntry = null;
+        for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
+            if (majorityEntry == null ||
+                    entry.getValue() > majorityEntry.getValue()) {
+                majorityEntry = entry;
+            }
+        }
+
+        return majorityEntry.getKey();
+    }
+
+    //排序方法
+    public int majorityElement1(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
+    }
 }
