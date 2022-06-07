bf368b8651cc6e515e86996e4040d902153569be
Pikachu <jiangjiaxin2011@126.com>
2022-06-07 23:59:07 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/linked_list/LC138_复制带随机指针的链表_mid.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/linked_list/LC138_复制带随机指针的链表_mid.java
@@ -1,4 +1,58 @@
 package cn.jjx.coding.leetcode.classification.linked_list;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LC138_复制带随机指针的链表_mid {
+
+    class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
+
+    Map<Node, Node> cachedNode = new HashMap<Node, Node>();
+
+    public Node copyRandomList(Node head) {
+        if (head == null) {
+            return null;
+        }
+        if (!cachedNode.containsKey(head)) {
+            Node headNew = new Node(head.val);
+            cachedNode.put(head, headNew);
+            headNew.next = copyRandomList(head.next);
+            headNew.random = copyRandomList(head.random);
+        }
+        return cachedNode.get(head);
+    }
+
+
+    public Node copyRandomList1(Node head) {
+        if (head == null) {
+            return null;
+        }
+        for (Node node = head; node != null; node = node.next.next) {
+            Node nodeNew = new Node(node.val);
+            nodeNew.next = node.next;
+            node.next = nodeNew;
+        }
+        for (Node node = head; node != null; node = node.next.next) {
+            Node nodeNew = node.next;
+            nodeNew.random = (node.random != null) ? node.random.next : null;
+        }
+        Node headNew = head.next;
+        for (Node node = head; node != null; node = node.next) {
+            Node nodeNew = node.next;
+            node.next = node.next.next;
+            nodeNew.next = (nodeNew.next != null) ? nodeNew.next.next : null;
+        }
+        return headNew;
+    }
+
 }
--- src/main/java/cn/jjx/coding/leetcode/classification/linked_list/LC86_分隔链表_mid.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/linked_list/LC86_分隔链表_mid.java
@@ -1,4 +1,26 @@
 package cn.jjx.coding.leetcode.classification.linked_list;
 
+import cn.jjx.coding.leetcode.data_structure.ListNode;
+
 public class LC86_分隔链表_mid {
+
+    public ListNode partition(ListNode head, int x) {
+        ListNode small = new ListNode(0);
+        ListNode smallHead = small;
+        ListNode large = new ListNode(0);
+        ListNode largeHead = large;
+        while (head != null) {
+            if (head.val < x) {
+                small.next = head;
+                small = small.next;
+            } else {
+                large.next = head;
+                large = large.next;
+            }
+            head = head.next;
+        }
+        large.next = null;
+        small.next = largeHead.next;
+        return smallHead.next;
+    }
 }
