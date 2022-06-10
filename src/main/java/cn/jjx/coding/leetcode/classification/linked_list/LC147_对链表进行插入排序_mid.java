1324986512d8c6a2281b621d632f4ea94fbb15dc
Pikachu <jiangjiaxin2011@126.com>
2022-06-11 00:02:37 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/linked_list/LC147_对链表进行插入排序_mid.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/linked_list/LC147_对链表进行插入排序_mid.java
@@ -1,4 +1,31 @@
 package cn.jjx.coding.leetcode.classification.linked_list;
 
+import cn.jjx.coding.leetcode.data_structure.ListNode;
+
 public class LC147_对链表进行插入排序_mid {
+
+    public ListNode insertionSortList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode lastSorted = head, curr = head.next;
+        while (curr != null) {
+            if (lastSorted.val <= curr.val) {
+                lastSorted = lastSorted.next;
+            } else {
+                ListNode prev = dummyHead;
+                while (prev.next.val <= curr.val) {
+                    prev = prev.next;
+                }
+                lastSorted.next = curr.next;
+                curr.next = prev.next;
+                prev.next = curr;
+            }
+            curr = lastSorted.next;
+        }
+        return dummyHead.next;
+    }
+
 }
