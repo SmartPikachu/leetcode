ddae50fbe034e34d1fdbf59dc1ba7a60157060bc
Pikachu <jiangjiaxin2011@126.com>
2022-06-12 11:34:03 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/linked_list/LC382_链表随机节点_mid.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/linked_list/LC382_链表随机节点_mid.java
@@ -1,4 +1,51 @@
 package cn.jjx.coding.leetcode.classification.linked_list;
 
+import cn.jjx.coding.leetcode.data_structure.ListNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class LC382_链表随机节点_mid {
+
+    //水塘抽样
+    class Solution {
+        ListNode head;
+        Random random;
+
+        public Solution(ListNode head) {
+            this.head = head;
+            random = new Random();
+        }
+
+        public int getRandom() {
+            int i = 1, ans = 0;
+            for (ListNode node = head; node != null; node = node.next) {
+                if (random.nextInt(i) == 0) { // 1/i 的概率选中（替换为答案）
+                    ans = node.val;
+                }
+                ++i;
+            }
+            return ans;
+        }
+    }
+
+    class Solution1 {
+        List<Integer> list;
+        Random random;
+
+        public Solution1(ListNode head) {
+            list = new ArrayList<Integer>();
+            while (head != null) {
+                list.add(head.val);
+                head = head.next;
+            }
+            random = new Random();
+        }
+
+        public int getRandom() {
+            return list.get(random.nextInt(list.size()));
+        }
+    }
+
 }
--- src/main/java/cn/jjx/coding/leetcode/classification/linked_list/LC426_将二叉搜索树转化为排序的双向链表_mid.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/linked_list/LC426_将二叉搜索树转化为排序的双向链表_mid.java
@@ -1,4 +1,62 @@
 package cn.jjx.coding.leetcode.classification.linked_list;
 
+import javax.xml.soap.Node;
+
 public class LC426_将二叉搜索树转化为排序的双向链表_mid {
+    
+    // the smallest (first) and the largest (last) nodes
+    Node first = null;
+    Node last = null;
+
+    //中序遍历二叉树搜索树，构造双向循环链表。
+    public void helper(Node node) {
+        if (node != null) {
+            // left
+            helper(node.left);
+            // node 
+            if (last != null) {
+                // link the previous node (last)
+                // with the current one (node)
+                last.right = node;
+                node.left = last;
+            }
+            else {
+                // keep the smallest node
+                // to close DLL later on
+                first = node;
+            }
+            last = node;
+            // right
+            helper(node.right);
+        }
+    }
+
+    public Node treeToDoublyList(Node root) {
+        if (root == null) return null;
+
+        helper(root);
+        // close DLL
+        last.right = first;
+        first.left = last;
+        return first;
+    }
+
+
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val,Node _left,Node _right) {
+            val = _val;
+            left = _left;
+            right = _right;
+        }
+    }
 }
