f043e3f8e8acecee3c655cb78a0d363d77d1d53b
Pikachu <jiangjiaxin2011@126.com>
2022-06-16 23:58:16 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/queue/OF_59_II_队列的最大值_mid.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/queue/OF_59_II_队列的最大值_mid.java
@@ -1,4 +1,46 @@
 package cn.jjx.coding.leetcode.classification.queue;
 
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class OF_59_II_队列的最大值_mid {
+
+    class MaxQueue {
+        Queue<Integer> q;
+        Deque<Integer> d;
+
+        public MaxQueue() {
+            q = new LinkedList<Integer>();
+            d = new LinkedList<Integer>();
+        }
+
+        public int max_value() {
+            if (d.isEmpty()) {
+                return -1;
+            }
+            return d.peekFirst();
+        }
+
+        public void push_back(int value) {
+            while (!d.isEmpty() && d.peekLast() < value) {
+                d.pollLast();
+            }
+            d.offerLast(value);
+            q.offer(value);
+        }
+
+        public int pop_front() {
+            if (q.isEmpty()) {
+                return -1;
+            }
+            int ans = q.poll();
+            if (ans == d.peekFirst()) {
+                d.pollFirst();
+            }
+            return ans;
+        }
+    }
+
+
 }
