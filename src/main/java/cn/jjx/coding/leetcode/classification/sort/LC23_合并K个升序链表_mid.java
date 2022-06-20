c3a8f773908a0d095080cb620190bc230cb3117d
Pikachu <jiangjiaxin2011@126.com>
2022-06-21 01:45:34 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/sort/LC23_合并K个升序链表_mid.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/sort/LC23_合并K个升序链表_mid.java
@@ -1,4 +1,86 @@
 package cn.jjx.coding.leetcode.classification.sort;
 
+import cn.jjx.coding.leetcode.data_structure.ListNode;
+
+import java.util.PriorityQueue;
+
 public class LC23_合并K个升序链表_mid {
+
+    //分治合并，常规的方法
+    class Solution {
+        public ListNode mergeKLists(ListNode[] lists) {
+            return merge(lists, 0, lists.length - 1);
+        }
+
+        public ListNode merge(ListNode[] lists, int l, int r) {
+            if (l == r) {
+                return lists[l];
+            }
+            if (l > r) {
+                return null;
+            }
+            int mid = (l + r) >> 1;
+            return mergeTwoLists(merge(lists, l, mid), merge(lists, mid + 1, r));
+        }
+
+        public ListNode mergeTwoLists(ListNode a, ListNode b) {
+            if (a == null || b == null) {
+                return a != null ? a : b;
+            }
+            ListNode head = new ListNode(0);
+            ListNode tail = head, aPtr = a, bPtr = b;
+            while (aPtr != null && bPtr != null) {
+                if (aPtr.val < bPtr.val) {
+                    tail.next = aPtr;
+                    aPtr = aPtr.next;
+                } else {
+                    tail.next = bPtr;
+                    bPtr = bPtr.next;
+                }
+                tail = tail.next;
+            }
+            tail.next = (aPtr != null ? aPtr : bPtr);
+            return head.next;
+        }
+    }
+
+    //采用优先队列的方法，本质是堆排序
+    class Solution1 {
+        class Status implements Comparable<Status> {
+            int val;
+            ListNode ptr;
+
+            Status(int val, ListNode ptr) {
+                this.val = val;
+                this.ptr = ptr;
+            }
+
+            public int compareTo(Status status2) {
+                return this.val - status2.val;
+            }
+        }
+
+        PriorityQueue<Status> queue = new PriorityQueue<>();
+
+        public ListNode mergeKLists(ListNode[] lists) {
+            for (ListNode node: lists) {
+                if (node != null) {
+                    queue.offer(new Status(node.val, node));
+                }
+            }
+            ListNode head = new ListNode(0);
+            ListNode tail = head;
+            while (!queue.isEmpty()) {
+                Status f = queue.poll();
+                tail.next = f.ptr;
+                tail = tail.next;
+                if (f.ptr.next != null) {
+                    queue.offer(new Status(f.ptr.next.val, f.ptr.next));
+                }
+            }
+            return head.next;
+        }
+    }
+
+
 }
--- src/main/java/cn/jjx/coding/leetcode/classification/sort/LC56_合并区间_mid.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/sort/LC56_合并区间_mid.java
@@ -1,4 +1,55 @@
 package cn.jjx.coding.leetcode.classification.sort;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+import static java.util.Comparator.comparingInt;
+
 public class LC56_合并区间_mid {
+
+    public int[][] merge(int[][] intervals) {
+        if (intervals.length == 0) {
+            return new int[0][2];
+        }
+
+        Arrays.sort(intervals, (interval1, interval2) -> interval1[0] - interval2[0]);
+        /**
+         * 这么写lanbada表达式更优雅
+         * Arrays.sort(intervals, Comparator.comparingInt((interval->interval[0])));
+         *
+         * //这个完全是匿名内部类
+         * Arrays.sort(intervals, new Comparator<int[]>() {
+         *             public int compare(int[] interval1, int[] interval2) {
+         *                 return interval1[0] - interval2[0];
+         *             }
+         *         });
+         */
+
+        List<int[]> merged = new ArrayList<>();
+        for(int i = 0; i < intervals.length; ++i){
+            int L = intervals[i][0], R = intervals[i][1];
+            if (merged.size() == 0 || merged.get(merged.size() - 1)[1] < L) {
+                merged.add(new int[]{L, R});
+            } else {
+                merged.get(merged.size() - 1)[1] =
+                        Math.max(merged.get(merged.size() - 1)[1], R);
+            }
+        }
+
+        /**
+         * 这种是利用流
+         * IntStream.range(0, intervals.length).forEach(i -> {
+         *             int L = intervals[i][0], R = intervals[i][1];
+         *             if (merged.size() == 0 || merged.get(merged.size() - 1)[1] < L) {
+         *                 merged.add(new int[]{L, R});
+         *             } else {
+         *                 merged.get(merged.size() - 1)[1] =
+         *                         Math.max(merged.get(merged.size() - 1)[1], R);
+         *             }
+         *         });
+         */
+        return merged.toArray(new int[merged.size()][]);
+    }
 }
