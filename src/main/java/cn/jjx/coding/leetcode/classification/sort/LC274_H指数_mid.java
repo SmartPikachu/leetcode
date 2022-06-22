7889594e324be83e45792a41a538392b767e5fe1
Pikachu <jiangjiaxin2011@126.com>
2022-06-22 23:50:36 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/sort/LC274_H指数_mid.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/sort/LC274_H指数_mid.java
@@ -1,4 +1,25 @@
 package cn.jjx.coding.leetcode.classification.sort;
 
 public class LC274_H指数_mid {
+
+    //计数排序
+    public int hIndex(int[] citations) {
+        int n = citations.length, tot = 0;
+        int[] counter = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            if (citations[i] >= n) {
+                counter[n]++;
+            } else {
+                counter[citations[i]]++;
+            }
+        }
+        for (int i = n; i >= 0; i--) {
+            tot += counter[i];
+            if (tot >= i) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
 }
--- src/main/java/cn/jjx/coding/leetcode/classification/sort/LC275_H指数II_mid.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/sort/LC275_H指数II_mid.java
@@ -1,4 +1,17 @@
 package cn.jjx.coding.leetcode.classification.sort;
 
 public class LC275_H指数II_mid {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        int left = 0, right = n - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (citations[mid] >= n - mid) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return n - left;
+    }
 }
