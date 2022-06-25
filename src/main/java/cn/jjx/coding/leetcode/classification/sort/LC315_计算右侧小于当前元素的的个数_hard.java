89b342a51bf0612cc5f60cf33ffecdbf151ff8ae
Pikachu <jiangjiaxin2011@126.com>
2022-06-25 23:43:32 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/sort/LC315_计算右侧小于当前元素的的个数_hard.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/sort/LC315_计算右侧小于当前元素的的个数_hard.java
@@ -1,4 +1,78 @@
 package cn.jjx.coding.leetcode.classification.sort;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LC315_计算右侧小于当前元素的的个数_hard {
+    //记录数组原始下标的
+    private int[] index;
+    //记录排序后的数组
+    private int[] temp;
+    //记录排序后数组的原始索引
+    private int[] tempIndex;
+    //记录结果的数组
+    private int[] ans;
+
+    public List<Integer> countSmaller(int[] nums) {
+        this.index = new int[nums.length];
+        this.temp = new int[nums.length];
+        this.tempIndex = new int[nums.length];
+        this.ans = new int[nums.length];
+        for (int i = 0; i < nums.length; ++i) {
+            index[i] = i;
+        }
+        int l = 0, r = nums.length - 1;
+        mergeSort(nums, l, r);
+        List<Integer> list = new ArrayList<Integer>();
+        for (int num : ans) {
+            list.add(num);
+        }
+        return list;
+    }
+
+    public void mergeSort(int[] a, int l, int r) {
+        if (l >= r) {
+            return;
+        }
+        int mid = (l + r) >> 1;
+        mergeSort(a, l, mid);
+        mergeSort(a, mid + 1, r);
+        merge(a, l, mid, r);
+    }
+
+    public void merge(int[] a, int l, int mid, int r) {
+        //这个p表示实际temp数组的指针，temp是个排序的数组
+        int i = l, j = mid + 1, p = l;
+        while (i <= mid && j <= r) {
+            if (a[i] <= a[j]) {
+                temp[p] = a[i];
+                tempIndex[p] = index[i];
+                ans[index[i]] += (j - mid - 1);
+                ++i;
+                ++p;
+            } else {
+                temp[p] = a[j];
+                tempIndex[p] = index[j];
+                ++j;
+                ++p;
+            }
+        }
+        while (i <= mid)  {
+            temp[p] = a[i];
+            tempIndex[p] = index[i];
+            ans[index[i]] += (j - mid - 1);
+            ++i;
+            ++p;
+        }
+        while (j <= r) {
+            temp[p] = a[j];
+            tempIndex[p] = index[j];
+            ++j;
+            ++p;
+        }
+        for (int k = l; k <= r; ++k) {
+            index[k] = tempIndex[k];
+            a[k] = temp[k];
+        }
+    }
 }
