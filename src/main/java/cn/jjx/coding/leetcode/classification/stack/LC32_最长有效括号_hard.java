709afa25d5583e59a17c5626c28cb06806b16bcf
Pikachu <jiangjiaxin2011@126.com>
2022-06-26 23:56:37 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/stack/LC32_最长有效括号_hard.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/stack/LC32_最长有效括号_hard.java
@@ -1,4 +1,45 @@
 package cn.jjx.coding.leetcode.classification.stack;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class LC32_最长有效括号_hard {
+
+    //动态规划
+    public int longestValidParentheses(String s) {
+        int maxans = 0;
+        int[] dp = new int[s.length()];
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == ')') {
+                if (s.charAt(i - 1) == '(') {
+                    dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+                } else if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+                    dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
+                }
+                maxans = Math.max(maxans, dp[i]);
+            }
+        }
+        return maxans;
+    }
+
+    //利用栈
+    public int longestValidParentheses1(String s) {
+        int maxans = 0;
+        Deque<Integer> stack = new LinkedList<>();
+        stack.push(-1);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                } else {
+                    maxans = Math.max(maxans, i - stack.peek());
+                }
+            }
+        }
+        return maxans;
+    }
+
 }
