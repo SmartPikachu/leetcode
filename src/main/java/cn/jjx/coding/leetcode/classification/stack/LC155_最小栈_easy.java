1356c2adf71d0401c30b5f20a641f425114d6900
Pikachu <jiangjiaxin2011@126.com>
2022-06-28 00:14:05 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/stack/LC155_最小栈_easy.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/stack/LC155_最小栈_easy.java
@@ -1,4 +1,37 @@
 package cn.jjx.coding.leetcode.classification.stack;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class LC155_最小栈_easy {
+
+    class MinStack {
+        Deque<Integer> xStack;
+        Deque<Integer> minStack;
+
+        public MinStack() {
+            xStack = new LinkedList<>();
+            minStack = new LinkedList<>();
+            minStack.push(Integer.MAX_VALUE);
+        }
+
+        public void push(int x) {
+            xStack.push(x);
+            minStack.push(Math.min(minStack.peek(), x));
+        }
+
+        public void pop() {
+            xStack.pop();
+            minStack.pop();
+        }
+
+        public int top() {
+            return xStack.peek();
+        }
+
+        public int getMin() {
+            return minStack.peek();
+        }
+    }
+
 }
