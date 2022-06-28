87ed2b4859328074958644072349cd6d9efaba95
Pikachu <jiangjiaxin2011@126.com>
2022-06-28 23:53:08 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/stack/LC946_验证栈序列_mid.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/stack/LC946_验证栈序列_mid.java
@@ -1,4 +1,23 @@
 package cn.jjx.coding.leetcode.classification.stack;
 
+import java.util.Stack;
+
 public class LC946_验证栈序列_mid {
+
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        int N = pushed.length;
+        Stack<Integer> stack = new Stack();
+
+        int j = 0;
+        for (int x: pushed) {
+            stack.push(x);
+            while (!stack.isEmpty() && j < N &&
+                    stack.peek() == popped[j]) {
+                stack.pop();
+                j++;
+            }
+        }
+
+        return j == N;
+    }
 }
