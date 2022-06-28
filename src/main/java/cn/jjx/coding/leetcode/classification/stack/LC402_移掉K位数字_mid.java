42e87fb636bd4dbf774b64a87d6a1746db8b6fb3
Pikachu <jiangjiaxin2011@126.com>
2022-06-28 23:33:52 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/stack/LC402_移掉K位数字_mid.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/stack/LC402_移掉K位数字_mid.java
@@ -1,2 +1,39 @@
-package cn.jjx.coding.leetcode.classification.stack;public class LC402_移掉K位数字_mid {
+package cn.jjx.coding.leetcode.classification.stack;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class LC402_移掉K位数字_mid {
+
+    //贪心算法+单调栈
+    public String removeKdigits(String num, int k) {
+        Deque<Character> deque = new LinkedList<>();
+        int length = num.length();
+        for (int i = 0; i < length; ++i) {
+            char digit = num.charAt(i);
+            while (!deque.isEmpty() && k > 0 && deque.peekLast() > digit) {
+                deque.pollLast();
+                k--;
+            }
+            deque.offerLast(digit);
+        }
+
+        for (int i = 0; i < k; ++i) {
+            deque.pollLast();
+        }
+
+        StringBuilder ret = new StringBuilder();
+        boolean leadingZero = true;
+        while (!deque.isEmpty()) {
+            char digit = deque.pollFirst();
+            if (leadingZero && digit == '0') {
+                continue;
+            }
+            leadingZero = false;
+            ret.append(digit);
+        }
+        return ret.length() == 0 ? "0" : ret.toString();
+    }
+
+
 }
--- src/main/java/cn/jjx/coding/leetcode/classification/stack/LC735_行星碰撞_mid.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/stack/LC735_行星碰撞_mid.java
@@ -1,4 +1,53 @@
 package cn.jjx.coding.leetcode.classification.stack;
 
+import java.util.Stack;
+
 public class LC735_行星碰撞_mid {
+
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack();
+        for (int ast: asteroids) {
+            collision: {
+                while (!stack.isEmpty() && ast < 0 && 0 < stack.peek()) {
+                    if (stack.peek() < -ast) {
+                        stack.pop();
+                        continue;
+                    } else if (stack.peek() == -ast) {
+                        stack.pop();
+                    }
+                    break collision;
+                }
+                stack.push(ast);
+            }
+        }
+
+        int[] ans = new int[stack.size()];
+        for (int t = ans.length - 1; t >= 0; --t) {
+            ans[t] = stack.pop();
+        }
+        return ans;
+    }
+
+
+    public int[] asteroidCollision1(int[] asteroids) {
+        Stack<Integer> s = new Stack<>();
+        int p = 0;
+        while (p < asteroids.length) {
+            if (s.empty() || s.peek() < 0 || asteroids[p] > 0) {
+                s.push(asteroids[p]);
+            } else if (s.peek() <= -asteroids[p]) {
+                if (s.pop() < -asteroids[p]) {
+                    continue;
+                }
+            }
+            p++;
+        }
+        int[] ret = new int[s.size()];
+        for (int i = ret.length - 1; i >= 0; i--) {
+            ret[i] = s.pop();
+        }
+        return ret;
+    }
+
+
 }
