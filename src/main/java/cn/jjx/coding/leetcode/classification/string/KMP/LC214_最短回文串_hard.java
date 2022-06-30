f8477f4af4e640102611e37429832c6d92755113
Pikachu <jiangjiaxin2011@126.com>
2022-06-30 23:35:55 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/string/KMP/LC214_最短回文串_hard.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/string/KMP/LC214_最短回文串_hard.java
@@ -1,4 +1,36 @@
 package cn.jjx.coding.leetcode.classification.string.KMP;
 
+import java.util.Arrays;
+
 public class LC214_最短回文串_hard {
+
+    //这个官方题解其实还是没搞明白。
+    public String shortestPalindrome(String s) {
+        int n = s.length();
+        int[] fail = new int[n];
+        Arrays.fill(fail, -1);
+        for (int i = 1; i < n; ++i) {
+            int j = fail[i - 1];
+            while (j != -1 && s.charAt(j + 1) != s.charAt(i)) {
+                j = fail[j];
+            }
+            if (s.charAt(j + 1) == s.charAt(i)) {
+                fail[i] = j + 1;
+            }
+        }
+        int best = -1;
+        for (int i = n - 1; i >= 0; --i) {
+            while (best != -1 && s.charAt(best + 1) != s.charAt(i)) {
+                best = fail[best];
+            }
+            if (s.charAt(best + 1) == s.charAt(i)) {
+                ++best;
+            }
+        }
+        String add = (best == n - 1 ? "" : s.substring(best + 1));
+        StringBuffer ans = new StringBuffer(add).reverse();
+        ans.append(s);
+        return ans.toString();
+    }
+
 }
--- src/main/java/cn/jjx/coding/leetcode/classification/string/KMP/LC261_以图判树_mid.java
+++
@@ -1,4 +0,0 @@
-package cn.jjx.coding.leetcode.classification.string.KMP;
-
-public class LC261_以图判树_mid {
-}
