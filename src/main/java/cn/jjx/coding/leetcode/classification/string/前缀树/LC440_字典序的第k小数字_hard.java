c7ff75c50b9ede5a8706afe59bdf2060dd23beb4
Pikachu <jiangjiaxin2011@126.com>
2022-06-30 23:36:30 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/string/前缀树/LC440_字典序的第k小数字_hard.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/string/前缀树/LC440_字典序的第k小数字_hard.java
@@ -1,4 +1,33 @@
 package cn.jjx.coding.leetcode.classification.string.前缀树;
 
 public class LC440_字典序的第k小数字_hard {
+
+    public int findKthNumber(int n, int k) {
+        int curr = 1;
+        k--;
+        while (k > 0) {
+            int steps = getSteps(curr, n);
+            if (steps <= k) {
+                k -= steps;
+                curr++;
+            } else {
+                curr = curr * 10;
+                k--;
+            }
+        }
+        return curr;
+    }
+
+    public int getSteps(int curr, long n) {
+        int steps = 0;
+        long first = curr;
+        long last = curr;
+        while (first <= n) {
+            steps += Math.min(last, n) - first + 1;
+            first = first * 10;
+            last = last * 10 + 9;
+        }
+        return steps;
+    }
+
 }
--- src/main/java/cn/jjx/coding/leetcode/classification/string/前缀树/LC677_键值映射_mid.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/string/前缀树/LC677_键值映射_mid.java
@@ -1,4 +1,47 @@
 package cn.jjx.coding.leetcode.classification.string.前缀树;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LC677_键值映射_mid {
+
+    class MapSum {
+        class TrieNode {
+            int val = 0;
+            TrieNode[] next = new TrieNode[26];
+        }
+
+        TrieNode root;
+        Map<String, Integer> map;
+
+        public MapSum() {
+            root = new TrieNode();
+            map = new HashMap<>();
+        }
+
+        public void insert(String key, int val) {
+            int delta = val - map.getOrDefault(key, 0);
+            map.put(key, val);
+            TrieNode node = root;
+            for (char c : key.toCharArray()) {
+                if (node.next[c - 'a'] == null) {
+                    node.next[c - 'a'] = new TrieNode();
+                }
+                node = node.next[c - 'a'];
+                node.val += delta;
+            }
+        }
+
+        public int sum(String prefix) {
+            TrieNode node = root;
+            for (char c : prefix.toCharArray()) {
+                if (node.next[c - 'a'] == null) {
+                    return 0;
+                }
+                node = node.next[c - 'a'];
+            }
+            return node.val;
+        }
+    }
+
 }
