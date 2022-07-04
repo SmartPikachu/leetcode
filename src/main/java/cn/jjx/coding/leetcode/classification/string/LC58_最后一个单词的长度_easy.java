6e152b9253bc5555b62e296115822d0fbdf2ea57
Pikachu <jiangjiaxin2011@126.com>
2022-07-04 20:56:11 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/string/LC58_最后一个单词的长度_easy.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/string/LC58_最后一个单词的长度_easy.java
@@ -1,4 +1,16 @@
 package cn.jjx.coding.leetcode.classification.string;
 
 public class LC58_最后一个单词的长度_easy {
+    public int lengthOfLastWord(String s) {
+        int index = s.length() - 1;
+        while (s.charAt(index) == ' ') {
+            index--;
+        }
+        int wordLength = 0;
+        while (index >= 0 && s.charAt(index) != ' ') {
+            wordLength++;
+            index--;
+        }
+        return wordLength;
+    }
 }
--- src/main/java/cn/jjx/coding/leetcode/classification/string/LC65_有效数字_hard.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/string/LC65_有效数字_hard.java
@@ -1,4 +1,149 @@
 package cn.jjx.coding.leetcode.classification.string;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LC65_有效数字_hard {
+
+    /**
+     * 先设定numSeen，dotSeen和eSeen三种boolean变量，分别代表是否出现数字、点和E
+     * 然后遍历目标字符串
+     * 1.判断是否属于数字的0~9区间
+     * 2.遇到点的时候，判断前面是否有点或者E，都需要return false
+     * 3.遇到E的时候，判断前面数字是否合理，是否有E，并把numSeen置为false，防止E后无数字
+     * 4.遇到-+的时候，判断是否是第一个，如果不是第一个判断是否在E后面，都不满足则return false
+     * 5.其他情况都为false
+     * 最后返回numSeen的结果即可
+     */
+
+    //采用模拟的方法，效率高，并且代码清晰
+    class Solution {
+        public boolean isNumber(String s) {
+            if(s==null||s.length()==0) return false;
+            boolean numSeen=false;
+            boolean dotSeen=false;
+            boolean eSeen=false;
+            char[] arr =s.trim().toCharArray();
+            for(int i=0; i<arr.length; i++){
+                if(arr[i]>='0'&&arr[i]<='9'){
+                    numSeen=true;
+                }else if(arr[i]=='.'){
+                    if(dotSeen||eSeen){
+                        return false;
+                    }
+                    dotSeen=true;
+                }else if(arr[i]=='E'||arr[i]=='e'){
+                    if(eSeen||!numSeen){
+                        return false;
+                    }
+                    eSeen=true;
+                    numSeen=false;
+                }else if(arr[i]=='+'||arr[i]=='-'){
+                    if(i!=0&&arr[i-1]!='e'&&arr[i-1]!='E'){
+                        return false;
+                    }
+                }else{
+                    return false;
+                }
+            }
+            return numSeen;
+        }
+    }
+
+
+    //状态机，这种解法虽然思路简单，但是代码有点蹂长。
+    public boolean isNumber(String s) {
+        Map<State, Map<CharType, State>> transfer = new HashMap<State, Map<CharType, State>>();
+        Map<CharType, State> initialMap = new HashMap<CharType, State>() {{
+            put(CharType.CHAR_NUMBER, State.STATE_INTEGER);
+            put(CharType.CHAR_POINT, State.STATE_POINT_WITHOUT_INT);
+            put(CharType.CHAR_SIGN, State.STATE_INT_SIGN);
+        }};
+        transfer.put(State.STATE_INITIAL, initialMap);
+        Map<CharType, State> intSignMap = new HashMap<CharType, State>() {{
+            put(CharType.CHAR_NUMBER, State.STATE_INTEGER);
+            put(CharType.CHAR_POINT, State.STATE_POINT_WITHOUT_INT);
+        }};
+        transfer.put(State.STATE_INT_SIGN, intSignMap);
+        Map<CharType, State> integerMap = new HashMap<CharType, State>() {{
+            put(CharType.CHAR_NUMBER, State.STATE_INTEGER);
+            put(CharType.CHAR_EXP, State.STATE_EXP);
+            put(CharType.CHAR_POINT, State.STATE_POINT);
+        }};
+        transfer.put(State.STATE_INTEGER, integerMap);
+        Map<CharType, State> pointMap = new HashMap<CharType, State>() {{
+            put(CharType.CHAR_NUMBER, State.STATE_FRACTION);
+            put(CharType.CHAR_EXP, State.STATE_EXP);
+        }};
+        transfer.put(State.STATE_POINT, pointMap);
+        Map<CharType, State> pointWithoutIntMap = new HashMap<CharType, State>() {{
+            put(CharType.CHAR_NUMBER, State.STATE_FRACTION);
+        }};
+        transfer.put(State.STATE_POINT_WITHOUT_INT, pointWithoutIntMap);
+        Map<CharType, State> fractionMap = new HashMap<CharType, State>() {{
+            put(CharType.CHAR_NUMBER, State.STATE_FRACTION);
+            put(CharType.CHAR_EXP, State.STATE_EXP);
+        }};
+        transfer.put(State.STATE_FRACTION, fractionMap);
+        Map<CharType, State> expMap = new HashMap<CharType, State>() {{
+            put(CharType.CHAR_NUMBER, State.STATE_EXP_NUMBER);
+            put(CharType.CHAR_SIGN, State.STATE_EXP_SIGN);
+        }};
+        transfer.put(State.STATE_EXP, expMap);
+        Map<CharType, State> expSignMap = new HashMap<CharType, State>() {{
+            put(CharType.CHAR_NUMBER, State.STATE_EXP_NUMBER);
+        }};
+        transfer.put(State.STATE_EXP_SIGN, expSignMap);
+        Map<CharType, State> expNumberMap = new HashMap<CharType, State>() {{
+            put(CharType.CHAR_NUMBER, State.STATE_EXP_NUMBER);
+        }};
+        transfer.put(State.STATE_EXP_NUMBER, expNumberMap);
+
+        int length = s.length();
+        State state = State.STATE_INITIAL;
+
+        for (int i = 0; i < length; i++) {
+            CharType type = toCharType(s.charAt(i));
+            if (!transfer.get(state).containsKey(type)) {
+                return false;
+            } else {
+                state = transfer.get(state).get(type);
+            }
+        }
+        return state == State.STATE_INTEGER || state == State.STATE_POINT || state == State.STATE_FRACTION || state == State.STATE_EXP_NUMBER;
+    }
+
+    public CharType toCharType(char ch) {
+        if (ch >= '0' && ch <= '9') {
+            return CharType.CHAR_NUMBER;
+        } else if (ch == 'e' || ch == 'E') {
+            return CharType.CHAR_EXP;
+        } else if (ch == '.') {
+            return CharType.CHAR_POINT;
+        } else if (ch == '+' || ch == '-') {
+            return CharType.CHAR_SIGN;
+        } else {
+            return CharType.CHAR_ILLEGAL;
+        }
+    }
+
+    enum State {
+        STATE_INITIAL,
+        STATE_INT_SIGN,
+        STATE_INTEGER,
+        STATE_POINT,
+        STATE_POINT_WITHOUT_INT,
+        STATE_FRACTION,
+        STATE_EXP,
+        STATE_EXP_SIGN,
+        STATE_EXP_NUMBER
+    }
+
+    enum CharType {
+        CHAR_NUMBER,
+        CHAR_EXP,
+        CHAR_POINT,
+        CHAR_SIGN,
+        CHAR_ILLEGAL
+    }
 }
