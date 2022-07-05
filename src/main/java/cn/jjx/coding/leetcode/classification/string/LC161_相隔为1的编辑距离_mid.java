38ddad567323a33b7745e454e28d889e930c3b80
Pikachu <jiangjiaxin2011@126.com>
2022-07-05 23:35:38 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/string/LC161_相隔为1的编辑距离_mid.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/string/LC161_相隔为1的编辑距离_mid.java
@@ -1,4 +1,44 @@
 package cn.jjx.coding.leetcode.classification.string;
 
 public class LC161_相隔为1的编辑距离_mid {
+
+    //看22年最新的题解，本身这个代码已经足够清晰了。
+    public boolean isOneEditDistance(String s, String t) {
+        int m = s.length(), n = t.length();
+        if (n - m == 1) {
+            return isOneInsert(s, t);
+        } else if (m - n == 1) {
+            return isOneInsert(t, s);
+        } else if (m == n) {
+            boolean foundDifference = false;
+            for (int i = 0; i < m; i++) {
+                if (s.charAt(i) != t.charAt(i)) {
+                    if (!foundDifference) {
+                        foundDifference = true;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+            return foundDifference;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isOneInsert(String shorter, String longer) {
+        int length1 = shorter.length(), length2 = longer.length();
+        int index1 = 0, index2 = 0;
+        while (index1 < length1 && index2 < length2) {
+            if (shorter.charAt(index1) == longer.charAt(index2)) {
+                index1++;
+            }
+            index2++;
+            if (index2 - index1 > 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
--- src/main/java/cn/jjx/coding/leetcode/classification/string/LC165_比较版本号_mid.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/string/LC165_比较版本号_mid.java
@@ -1,4 +1,26 @@
 package cn.jjx.coding.leetcode.classification.string;
 
 public class LC165_比较版本号_mid {
+
+    public int compareVersion(String version1, String version2) {
+        int n = version1.length(), m = version2.length();
+        int i = 0, j = 0;
+        while (i < n || j < m) {
+            int x = 0;
+            for (; i < n && version1.charAt(i) != '.'; ++i) {
+                x = x * 10 + version1.charAt(i) - '0';
+            }
+            ++i; // 跳过点号
+            int y = 0;
+            for (; j < m && version2.charAt(j) != '.'; ++j) {
+                y = y * 10 + version2.charAt(j) - '0';
+            }
+            ++j; // 跳过点号
+            if (x != y) {
+                return x > y ? 1 : -1;
+            }
+        }
+        return 0;
+    }
+
 }
