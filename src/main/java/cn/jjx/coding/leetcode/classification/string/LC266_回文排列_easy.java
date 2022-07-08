33e2d21ffa26e5c3ce90507c930253d95411a82d
Pikachu <jiangjiaxin2011@126.com>
2022-07-08 13:35:01 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/string/LC266_回文排列_easy.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/string/LC266_回文排列_easy.java
@@ -1,5 +1,59 @@
 package cn.jjx.coding.leetcode.classification.string;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
 public class LC266_回文排列_easy {
+
+    //利用hashmap
+    public boolean canPermutePalindrome(String s) {
+        HashMap< Character, Integer > map = new HashMap < > ();
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        }
+        int count = 0;
+        for (char key: map.keySet()) {
+            count += map.get(key) % 2;
+        }
+        return count <= 1;
+    }
+
+    //利用数组
+    public boolean canPermutePalindrome1(String s) {
+        int[] map = new int[128];
+        for (int i = 0; i < s.length(); i++) {
+            map[s.charAt(i)]++;
+        }
+        int count = 0;
+        for (int key = 0; key < map.length && count <= 1; key++) {
+            count += map[key] % 2;
+        }
+        return count <= 1;
+    }
+
+    //利用数组
+    public boolean canPermutePalindrome2(String s) {
+        int[] map = new int[128];
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            map[s.charAt(i)]++;
+            if (map[s.charAt(i)] % 2 == 0)
+                count--;
+            else
+                count++;
+        }
+        return count <= 1;
+    }
+
+    //利用set集合
+    public boolean canPermutePalindrome4(String s) {
+        Set< Character > set = new HashSet< >();
+        for (int i = 0; i < s.length(); i++) {
+            if (!set.add(s.charAt(i)))
+                set.remove(s.charAt(i));
+        }
+        return set.size() <= 1;
+    }
 
 }
