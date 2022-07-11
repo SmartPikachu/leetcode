6995c9df76df218a112bb918133babdf61e29bb7
Pikachu <jiangjiaxin2011@126.com>
2022-07-11 09:05:43 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/tree/LC114_二叉树展开为链表_mid.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/tree/LC114_二叉树展开为链表_mid.java
@@ -1,4 +1,59 @@
 package cn.jjx.coding.leetcode.classification.tree;
 
+import cn.jjx.coding.leetcode.data_structure.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
 public class LC114_二叉树展开为链表_mid {
+
+    //采用递归的方法
+    class Solution {
+        public void flatten(TreeNode root) {
+            List<TreeNode> list = new ArrayList<TreeNode>();
+            preorderTraversal(root, list);
+            int size = list.size();
+            for (int i = 1; i < size; i++) {
+                TreeNode prev = list.get(i - 1), curr = list.get(i);
+                prev.left = null;
+                prev.right = curr;
+            }
+        }
+
+        public void preorderTraversal(TreeNode root, List<TreeNode> list) {
+            if (root != null) {
+                list.add(root);
+                preorderTraversal(root.left, list);
+                preorderTraversal(root.right, list);
+            }
+        }
+    }
+
+
+    //采用迭代的方法
+    class Solution1 {
+        public void flatten(TreeNode root) {
+            List<TreeNode> list = new ArrayList<TreeNode>();
+            Deque<TreeNode> stack = new LinkedList<TreeNode>();
+            TreeNode node = root;
+            while (node != null || !stack.isEmpty()) {
+                while (node != null) {
+                    list.add(node);
+                    stack.push(node);
+                    node = node.left;
+                }
+                node = stack.pop();
+                node = node.right;
+            }
+            int size = list.size();
+            for (int i = 1; i < size; i++) {
+                TreeNode prev = list.get(i - 1), curr = list.get(i);
+                prev.left = null;
+                prev.right = curr;
+            }
+        }
+    }
+
 }
