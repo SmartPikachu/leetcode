d04bdc6290874a9b81bc0d28096710144af55f1d
Pikachu <jiangjiaxin2011@126.com>
2022-07-11 14:12:42 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/tree/LC298_二叉树最长连续序列_hard.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/tree/LC298_二叉树最长连续序列_hard.java
@@ -35,28 +35,4 @@ public class LC298_二叉树最长连续序列_hard {
     }
 
 
-    //采用自底向上的深度优先搜索
-    class Solution1{
-        private int maxLength = 0;
-        public int longestConsecutive(TreeNode root) {
-            dfs(root);
-            return maxLength;
-        }
-
-        private int dfs(TreeNode p) {
-            if (p == null) return 0;
-            int L = dfs(p.left) + 1;
-            int R = dfs(p.right) + 1;
-            if (p.left != null && p.val + 1 != p.left.val) {
-                L = 1;
-            }
-            if (p.right != null && p.val + 1 != p.right.val) {
-                R = 1;
-            }
-            int length = Math.max(L, R);
-            maxLength = Math.max(maxLength, length);
-            return length;
-        }
-    }
-
 }
