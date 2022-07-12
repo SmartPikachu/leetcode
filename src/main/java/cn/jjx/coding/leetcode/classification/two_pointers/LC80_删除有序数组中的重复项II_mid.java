806f5e5d37d15a473720149191bc7643005c9ff8
Pikachu <jiangjiaxin2011@126.com>
2022-07-12 08:54:36 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/two_pointers/LC80_删除有序数组中的重复项II_mid.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/two_pointers/LC80_删除有序数组中的重复项II_mid.java
@@ -1,4 +1,21 @@
 package cn.jjx.coding.leetcode.classification.two_pointers;
 
 public class LC80_删除有序数组中的重复项II_mid {
+
+    public int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        if (n <= 2) {
+            return n;
+        }
+        int slow = 2, fast = 2;
+        while (fast < n) {
+            if (nums[slow - 2] != nums[fast]) {
+                nums[slow] = nums[fast];
+                ++slow;
+            }
+            ++fast;
+        }
+        return slow;
+    }
+
 }
