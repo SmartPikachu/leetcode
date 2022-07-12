b643e9df0c6a15909b389fb770a694e05526997c
Pikachu <jiangjiaxin2011@126.com>
2022-07-12 09:36:26 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/two_pointers/LC283_移动零_easy.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/two_pointers/LC283_移动零_easy.java
@@ -1,4 +1,22 @@
 package cn.jjx.coding.leetcode.classification.two_pointers;
 
 public class LC283_移动零_easy {
+
+    public void moveZeroes(int[] nums) {
+        int n = nums.length, left = 0, right = 0;
+        while (right < n) {
+            if (nums[right] != 0) {
+                swap(nums, left, right);
+                left++;
+            }
+            right++;
+        }
+    }
+
+    public void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+    }
+
 }
