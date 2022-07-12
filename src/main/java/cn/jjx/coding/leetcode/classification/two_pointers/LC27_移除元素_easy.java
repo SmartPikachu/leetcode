4b7fe92bfc98a33414dad3408202d7fa9d800602
Pikachu <jiangjiaxin2011@126.com>
2022-07-12 08:45:57 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/two_pointers/LC27_移除元素_easy.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/two_pointers/LC27_移除元素_easy.java
@@ -1,4 +1,31 @@
 package cn.jjx.coding.leetcode.classification.two_pointers;
 
 public class LC27_移除元素_easy {
+
+    public int removeElement(int[] nums, int val) {
+        int left = 0;
+        int right = nums.length;
+        while (left < right) {
+            if (nums[left] == val) {
+                nums[left] = nums[right - 1];
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return left;
+    }
+
+    public int removeElement1(int[] nums, int val) {
+        int n = nums.length;
+        int left = 0;
+        for (int right = 0; right < n; right++) {
+            if (nums[right] != val) {
+                nums[left] = nums[right];
+                left++;
+            }
+        }
+        return left;
+    }
+
 }
