cdfba5bb4f6713f892774010e56318a821eae8c8
Pikachu <jiangjiaxin2011@126.com>
2022-07-12 09:28:28 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/two_pointers/LC209_长度最小的子数组_mid.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/two_pointers/LC209_长度最小的子数组_mid.java
@@ -1,4 +1,60 @@
 package cn.jjx.coding.leetcode.classification.two_pointers;
 
+import java.util.Arrays;
+
 public class LC209_长度最小的子数组_mid {
+
+    //采用滑动窗口的方法，思路清晰，可以直接采用这种方法。
+    public int minSubArrayLen(int s, int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int ans = Integer.MAX_VALUE;
+        int start = 0, end = 0;
+        int sum = 0;
+        while (end < n) {
+            sum += nums[end];
+            while (sum >= s) {
+                ans = Math.min(ans, end - start + 1);
+                sum -= nums[start];
+                start++;
+            }
+            end++;
+        }
+        return ans == Integer.MAX_VALUE ? 0 : ans;
+    }
+
+
+    //前缀和 + 二分查找,官方题解已经写得很清楚了。
+    class Solution {
+        public int minSubArrayLen(int s, int[] nums) {
+            int n = nums.length;
+            if (n == 0) {
+                return 0;
+            }
+            int ans = Integer.MAX_VALUE;
+            int[] sums = new int[n + 1];
+            // 为了方便计算，令 size = n + 1
+            // sums[0] = 0 意味着前 0 个元素的前缀和为 0
+            // sums[1] = A[0] 前 1 个元素的前缀和为 A[0]
+            // 以此类推
+            for (int i = 1; i <= n; i++) {
+                sums[i] = sums[i - 1] + nums[i - 1];
+            }
+            for (int i = 1; i <= n; i++) {
+                int target = s + sums[i - 1];
+                //这个如果找不到返回-1
+                int bound = Arrays.binarySearch(sums, target);
+                if (bound < 0) {
+                    bound = -bound - 1;
+                }
+                if (bound <= n) {
+                    ans = Math.min(ans, bound - (i - 1));
+                }
+            }
+            return ans == Integer.MAX_VALUE ? 0 : ans;
+        }
+    }
+
 }
