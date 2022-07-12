42b03bd56847b7e788f6404aa659677393c28764
Pikachu <jiangjiaxin2011@126.com>
2022-07-12 09:39:18 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/two_pointers/OF_57_II_和为s的连续正数序列_easy.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/two_pointers/OF_57_II_和为s的连续正数序列_easy.java
@@ -1,4 +1,29 @@
 package cn.jjx.coding.leetcode.classification.two_pointers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class OF_57_II_和为s的连续正数序列_easy {
+
+    //采用双指针的方法
+    public int[][] findContinuousSequence(int target) {
+        List<int[]> vec = new ArrayList<int[]>();
+        for (int l = 1, r = 2; l < r;) {
+            int sum = (l + r) * (r - l + 1) / 2;
+            if (sum == target) {
+                int[] res = new int[r - l + 1];
+                for (int i = l; i <= r; ++i) {
+                    res[i - l] = i;
+                }
+                vec.add(res);
+                l++;
+            } else if (sum < target) {
+                r++;
+            } else {
+                l++;
+            }
+        }
+        return vec.toArray(new int[vec.size()][]);
+    }
+
 }
