97530b04fcc8b0b1dd01ca744749bebb3dff1a15
Pikachu <jiangjiaxin2011@126.com>
2022-07-14 19:49:52 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/array/LC74_搜索二维矩阵_mid.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/array/LC74_搜索二维矩阵_mid.java
@@ -2,6 +2,11 @@ package cn.jjx.coding.leetcode.classification.array;
 
 public class LC74_搜索二维矩阵_mid {
 
+    /**
+     * 若将矩阵每一行拼接在上一行的末尾，则会得到一个升序数组，
+     * 我们可以在该数组上二分找到目标元素。
+     * 代码实现时，可以二分升序数组的下标，将其映射到原矩阵的行和列上。
+     */
     public boolean searchMatrix(int[][] matrix, int target) {
         int m = matrix.length, n = matrix[0].length;
         int low = 0, high = m * n - 1;
