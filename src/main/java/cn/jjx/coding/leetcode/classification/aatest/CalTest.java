03af2ce816494e5a95f7944799c34356052909a1
Pikachu <jiangjiaxin2011@126.com>
2022-07-19 13:07:09 +0800

    提交代码

---
+++ src/main/java/cn/jjx/coding/leetcode/classification/aatest/CalTest.java
@@ -0,0 +1,8 @@
+package cn.jjx.coding.leetcode.classification.aatest;
+
+public class CalTest {
+    public static void main(String[] args) {
+        int a1 = 5;
+        System.out.println(5/2);
+    }
+}
