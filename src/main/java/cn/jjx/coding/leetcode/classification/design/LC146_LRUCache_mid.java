c01cdd17f10ca05dc775c4b10dcebc9af2727bd2
Pikachu <jiangjiaxin2011@126.com>
2022-07-19 23:03:47 +0800

    提交代码

--- src/main/java/cn/jjx/coding/leetcode/classification/design/LC146_LRUCache_mid.java
+++ src/main/java/cn/jjx/coding/leetcode/classification/design/LC146_LRUCache_mid.java
@@ -1,10 +1,57 @@
 package cn.jjx.coding.leetcode.classification.design;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class LC146_LRUCache_mid {
 
+    //采用labuladong的方法，利用java内置LinkedHashMap来实现。
+    //代码简洁，但是没有自己定义链表，还是有些取巧的成分。
+    class LRUCache {
+        int cap;
+        LinkedHashMap<Integer, Integer> cache = new LinkedHashMap<>();
+        public LRUCache(int capacity) {
+            this.cap = capacity;
+        }
+
+        public int get(int key) {
+            if (!cache.containsKey(key)) {
+                return -1;
+            }
+            // 将 key 变为最近使用
+            makeRecently(key);
+            return cache.get(key);
+        }
+
+        public void put(int key, int val) {
+            if (cache.containsKey(key)) {
+                // 修改 key 的值
+                cache.put(key, val);
+                // 将 key 变为最近使用
+                makeRecently(key);
+                return;
+            }
+
+            if (cache.size() >= this.cap) {
+                // 链表头部就是最久未使用的 key
+                int oldestKey = cache.keySet().iterator().next();
+                cache.remove(oldestKey);
+            }
+            // 将新的 key 添加链表尾部
+            cache.put(key, val);
+        }
+
+        private void makeRecently(int key) {
+            int val = cache.get(key);
+            // 删除 key，重新插入到队尾
+            cache.remove(key);
+            cache.put(key, val);
+        }
+    }
+
+
+    //官方题解，自己定义链表来实现。
     class DLinkedNode{
         int key;
         int value;
